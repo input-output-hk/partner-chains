3566f798e2880b0c5301297decc9d7f38726b3ac
Nikolaos Dymitriadis <nikolaos.dymitriadis@iohk.io>
2024-08-13 11:59:39 +0200

    provide expected length
    Signed-off-by: Nikolaos Dymitriadis <nikolaos.dymitriadis@iohk.io>

--- primitives/domain/src/crypto.rs
+++ primitives/domain/src/crypto.rs
@@ -1,8 +1,10 @@
+use alloc::format;
+
 pub fn blake2b<const N: usize>(data: &[u8]) -> [u8; N] {
 	blake2b_simd::Params::new()
 		.hash_length(N)
 		.hash(data)
 		.as_bytes()
 		.try_into()
-		.expect("hash output always has expected length")
+		.expect(&format!("hash output always has expected length of {N}"))
 }
