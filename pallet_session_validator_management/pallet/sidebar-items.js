733b6202e1b8fe974ab4f6d66918fb341939cba9
LGLO <LGLO@users.noreply.github.com>
2024-09-18 07:36:56 +0000

    deploy: 16049036637a6714fbf5a3eea46561d6e3d0add2

--- pallet_session_validator_management/pallet/sidebar-items.js
+++ pallet_session_validator_management/pallet/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Call","Error","Event"],"mod":["dispatchables","storage_types"],"struct":["CommitteeInfo","GenesisConfig","OptionIdentity","Pallet"],"trait":["Config"],"type":["CurrentCommittee","MainChainScriptsConfiguration","Module","NextCommittee"]};
+window.SIDEBAR_ITEMS = {"enum":["Call","Error","Event"],"mod":["dispatchables","storage_types"],"struct":["CommitteeInfo","GenesisConfig","Pallet"],"trait":["Config"],"type":["CurrentCommittee","MainChainScriptsConfiguration","Module","NextCommittee"]};
