f0eae4515d8067886357e64c7850021e05ec6a58
Larry Adames <larry.adames@iohk.io>
2024-09-24 10:34:57 -0400

    v1.2.0 release - set smart contracts version

--- flake.nix
+++ flake.nix
@@ -24,8 +24,7 @@
 
     # Partner Chains deps
     smart-contracts = {
-      # Update to desired tag when ready for release
-      url = "github:input-output-hk/partner-chains-smart-contracts";
+      url = "github:input-output-hk/partner-chains-smart-contracts/v6.1.0";
       flake = false;
     };
     cardano-node = {
