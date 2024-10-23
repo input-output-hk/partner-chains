03724b0693ca967024b18c2b61e7e8689be44dbb
Lech Głowiak <lech.glowiak@iohk.io>
2024-10-23 09:14:21 +0200

    Fix transactions and add tests for it

--- utils/ogmios-client/src/transactions.rs
+++ utils/ogmios-client/src/transactions.rs
@@ -1,7 +1,7 @@
 //! Requests to evalute and submit transactions via Ogmios`.
 
-use crate::{ByNameParamsBuilder, OgmiosClient, OgmiosClientError};
-use serde::{Deserialize, Deserializer};
+use crate::{types::OgmiosTx, ByNameParamsBuilder, OgmiosClient, OgmiosClientError};
+use serde::Deserialize;
 
 pub trait Transactions: OgmiosClient {
 	/// Evaluates a transaction.
@@ -61,16 +61,5 @@ pub struct OgmiosBudget {
 
 #[derive(Clone, Debug, Deserialize, PartialEq, Eq)]
 pub struct SubmitTransactionResponse {
-	#[serde(deserialize_with = "parse_tx_id")]
-	pub id: [u8; 32],
-}
-
-fn parse_tx_id<'de, D>(deserializer: D) -> Result<[u8; 32], D::Error>
-where
-	D: Deserializer<'de>,
-{
-	let buf = String::deserialize(deserializer)?;
-	let vec = hex::decode(buf).map_err(serde::de::Error::custom)?;
-	TryFrom::try_from(vec)
-		.map_err(|e| serde::de::Error::custom(format!("{} has invalid size", hex::encode(e))))
+	pub transaction: OgmiosTx,
 }
---
+++ utils/ogmios-client/tests/transactions.rs
@@ -0,0 +1,73 @@
+#![cfg(feature = "jsonrpsee-client")]
+
+use hex_literal::hex;
+use jsonrpsee::{http_client::HttpClient, types::ErrorCode};
+use ogmios_client::{
+	transactions::{
+		OgmiosBudget, OgmiosEvaluateTransactionResponse, OgmiosValidatorIndex,
+		SubmitTransactionResponse, Transactions,
+	},
+	types::OgmiosTx,
+};
+use serde_json::json;
+
+mod server;
+
+#[tokio::test]
+async fn evaluate_transaction() {
+	let address = server::for_single_test("evaluateTransaction", |req| {
+		let expected_params = json!({"transaction": { "cbor": "aabbccdd" }, "additionalUtxo": []});
+		let params_value: serde_json::Value = req.parse().unwrap();
+		if params_value == expected_params {
+			Ok(json!([{
+			  "validator": {
+				"index": 0,
+				"purpose": "spend"
+			  },
+			  "budget": {
+				"memory": 202586,
+				"cpu": 43653414
+			  }
+			}]))
+		} else {
+			Err(ErrorCode::InvalidParams.into())
+		}
+	})
+	.await
+	.unwrap();
+	let client = HttpClient::builder().build(format!("http://{address}")).unwrap();
+	let era_summaries = client.evaluate_transaction(&hex!("aabbccdd")).await.unwrap();
+	assert_eq!(
+		era_summaries[0],
+		OgmiosEvaluateTransactionResponse {
+			validator: OgmiosValidatorIndex { index: 0, purpose: "spend".into() },
+			budget: OgmiosBudget { memory: 202586, cpu: 43653414 },
+		}
+	)
+}
+
+#[tokio::test]
+async fn submit_transaction() {
+	let address = server::for_single_test("submitTransaction", |req| {
+	let expected_params = json!({"transaction": { "cbor": "aabbccdd" }});
+	let params_value: serde_json::Value = req.parse().unwrap();
+	if params_value == expected_params {
+		Ok(json!({"transaction": { "id": "e4891cd4e45c320301fff691bbd1bee0cf4484fc2ddc26c08c555d08efbb7d6b" }}))
+	} else {
+		Err(ErrorCode::InvalidParams.into())
+	}
+
+	})
+	.await
+	.unwrap();
+	let client = HttpClient::builder().build(format!("http://{address}")).unwrap();
+	let response = client.submit_transaction(&hex!("aabbccdd")).await.unwrap();
+	assert_eq!(
+		response,
+		SubmitTransactionResponse {
+			transaction: OgmiosTx {
+				id: hex!("e4891cd4e45c320301fff691bbd1bee0cf4484fc2ddc26c08c555d08efbb7d6b")
+			}
+		}
+	)
+}
