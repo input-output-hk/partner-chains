18a3cd3a4a9116382e15b537efac8c3ce261aaa1
Skylar Simoncelli <skylar@skylar.email>
2024-10-28 11:36:47 +0100

    feat: docker socket hack

--- dev/local-environment/configurations/tests/entrypoint.sh
+++ dev/local-environment/configurations/tests/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Verify the e2e-tests directory exists and cd into it
 if [ -d "/e2e-tests" ]; then
   cd /e2e-tests
 else
@@ -7,6 +8,10 @@ else
   exit 1
 fi
 
+# Install Docker CLI for running Docker commands in other containers
+apt-get update && apt-get install -y docker.io
+
+# Install pytest dependencies
 apt-get update && \
 apt-get install -y curl && \
 curl -L --silent https://github.com/getsops/sops/releases/download/v3.7.3/sops_3.7.3_amd64.deb > sops.deb && \
@@ -15,10 +20,11 @@ rm sops.deb && \
 apt-get clean && \
 rm -rf /var/lib/apt/lists/*
 
+# Create and initialize the virtual environment
 python -m venv venv
 source venv/bin/activate
-
 pip install --upgrade pip
 pip install -r requirements.txt
 
+# Keep the container running
 tail -f /dev/null
