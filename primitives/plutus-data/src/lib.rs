8d8a1b791d74f611447cf43a1cdbd9bc8786adc0
Nikolaos Dymitriadis <nikolaos.dymitriadis@iohk.io>
2024-11-05 16:27:06 +0100

    version oracle plutus data

--- primitives/plutus-data/src/lib.rs
+++ primitives/plutus-data/src/lib.rs
@@ -3,6 +3,7 @@ use cardano_serialization_lib::PlutusData;
 pub mod d_param;
 pub mod permissioned_candidates;
 pub mod registered_candidates;
+pub mod version_oracle;
 
 #[derive(Debug, PartialEq, thiserror::Error)]
 #[error("Could not decode {datum:?} to {to}: {msg}")]
@@ -16,11 +17,15 @@ type DecodingResult<T> = std::result::Result<T, DataDecodingError>;
 
 pub trait PlutusDataExtensions {
 	fn as_u16(self) -> Option<u16>;
+	fn as_u32(self) -> Option<u32>;
 }
 
 impl PlutusDataExtensions for cardano_serialization_lib::PlutusData {
 	fn as_u16(self) -> Option<u16> {
-		u16::try_from(u32::try_from(self.as_integer()?.as_u64()?).ok()?).ok()
+		u16::try_from(self.as_u32()?).ok()
+	}
+	fn as_u32(self) -> Option<u32> {
+		u32::try_from(self.as_integer()?.as_u64()?).ok()
 	}
 }
 
