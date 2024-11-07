bdd2fbf8aad8360a85e7086c2f467df4980c4b81
Taylor Gunnoe <taylor.gunnoe@iohk.io>
2024-11-07 16:04:57 -0500

    fix: fix cardano-cli call

--- nix/shell.nix
+++ nix/shell.nix
@@ -98,7 +98,7 @@
             name = "cardano-cli";
             help = "CLI v10.1.2 that is used in partner-chains dependency stack";
             # This command has some eval because of IFD
-            command = "${self'.packages.cardano-cli}/bin/cardano-cli latest $@";
+            command = "${self'.packages.cardano-cli}/bin/cardano-cli $@";
           }
         ];
       }
