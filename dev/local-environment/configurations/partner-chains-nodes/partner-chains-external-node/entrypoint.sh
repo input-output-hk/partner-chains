9911e390a58b8b124ca99b70905fb964d9a127f0
Skylar Simoncelli <skylar@skylar.email>
2024-11-19 14:09:32 +0100

    fix: entrypoint

--- dev/local-environment/configurations/partner-chains-nodes/partner-chains-external-node/entrypoint.sh
+++ dev/local-environment/configurations/partner-chains-nodes/partner-chains-external-node/entrypoint.sh
@@ -20,8 +20,8 @@ export NATIVE_TOKEN_POLICY_ID=$(cat /shared/NATIVE_TOKEN_POLICY_ID)
 export NATIVE_TOKEN_ASSET_NAME=$(cat /shared/NATIVE_TOKEN_ASSET_NAME)
 export ILLIQUID_SUPPLY_VALIDATOR_ADDRESS=$(cat /shared/ILLIQUID_SUPPLY_VALIDATOR_ADDRESS)
 
-# Check NODE_PROFILE and execute the corresponding command
-case "$NODE_PROFILE" in
+# Check ACCOUNT and execute the corresponding command
+case "$ACCOUNT" in
   alice)
     /usr/local/bin/partner-chains-node \
       --alice \
@@ -41,7 +41,7 @@ case "$NODE_PROFILE" in
       --bob \
       --chain=/shared/chain-spec.json \
       --node-key=0000000000000000000000000000000000000000000000000000000000000002 \
-      --bootnodes="/dns/partner-chains-node-1/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp" \
+      --bootnodes="/ip4/3.70.234.116/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp" \
       --base-path=/data \
       --unsafe-rpc-external \
       --rpc-port=9934 \
@@ -56,7 +56,7 @@ case "$NODE_PROFILE" in
       --charlie \
       --chain=/shared/chain-spec.json \
       --node-key=0000000000000000000000000000000000000000000000000000000000000003 \
-      --bootnodes="/dns/partner-chains-node-1/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp" \
+      --bootnodes="/ip4/3.70.234.116/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp" \
       --base-path=/data \
       --unsafe-rpc-external \
       --rpc-port=9935 \
@@ -66,8 +66,56 @@ case "$NODE_PROFILE" in
       --state-pruning=archive \
       --blocks-pruning=archive &
     ;;
+  dave)
+    /usr/local/bin/partner-chains-node \
+      --chain=/shared/chain-spec.json \
+      --validator \
+      --node-key=0000000000000000000000000000000000000000000000000000000000000004 \
+      --bootnodes="/ip4/3.70.234.116/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp" \
+      --base-path=/data \
+      --keystore-path=/keystore \
+      --unsafe-rpc-external \
+      --rpc-port=9936 \
+      --rpc-cors=all \
+      --prometheus-port=9618 \
+      --prometheus-external \
+      --state-pruning=archive \
+      --blocks-pruning=archive &
+    ;;
+  eve)
+    /usr/local/bin/partner-chains-node \
+      --chain=/shared/chain-spec.json \
+      --validator \
+      --node-key=0000000000000000000000000000000000000000000000000000000000000005 \
+      --bootnodes="/ip4/3.70.234.116/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp" \
+      --base-path=/data \
+      --keystore-path=/keystore \
+      --unsafe-rpc-external \
+      --rpc-port=9937 \
+      --rpc-cors=all \
+      --prometheus-port=9619 \
+      --prometheus-external \
+      --state-pruning=archive \
+      --blocks-pruning=archive &
+    ;;
+  ferdie)
+    /usr/local/bin/partner-chains-node \
+      --chain=/shared/chain-spec.json \
+      --validator \
+      --node-key=0000000000000000000000000000000000000000000000000000000000000006 \
+      --bootnodes="/ip4/3.70.234.116/tcp/30333/p2p/12D3KooWEyoppNCUx8Yx66oV9fJnriXwCcXwDDUA2kj6vnc6iDEp" \
+      --base-path=/data \
+      --keystore-path=/keystore \
+      --unsafe-rpc-external \
+      --rpc-port=9937 \
+      --rpc-cors=all \
+      --prometheus-port=9619 \
+      --prometheus-external \
+      --state-pruning=archive \
+      --blocks-pruning=archive &
+    ;;
   *)
-    echo "Error: Unknown NODE_PROFILE value '$NODE_PROFILE'. Please set NODE_PROFILE to 'alice', 'bob', or 'charlie' in your .envrc."
+    echo "Error: Unknown ACCOUNT value '$ACCOUNT'. Please set ACCOUNT to 'alice', 'bob', 'charlie', 'dave', 'eve', or 'ferdie' in your .envrc."
     exit 1
     ;;
 esac
