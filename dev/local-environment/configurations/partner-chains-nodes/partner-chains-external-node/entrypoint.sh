0feddbf149a700acb4cf6fe7acf4bb8f6bde8a5e
Skylar Simoncelli <skylar@skylar.email>
2024-11-20 11:37:52 +0100

    fix: vars

--- dev/local-environment/configurations/partner-chains-nodes/partner-chains-external-node/entrypoint.sh
+++ dev/local-environment/configurations/partner-chains-nodes/partner-chains-external-node/entrypoint.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-
-export MC__FIRST_EPOCH_TIMESTAMP_MILLIS=$(cat /shared/MC__FIRST_EPOCH_TIMESTAMP_MILLIS)
-export COMMITTEE_CANDIDATE_ADDRESS=$(cat /shared/COMMITTEE_CANDIDATE_ADDRESS)
-export D_PARAMETER_POLICY_ID=$(cat /shared/D_PARAMETER_POLICY_ID)
-export PERMISSIONED_CANDIDATES_POLICY_ID=$(cat /shared/PERMISSIONED_CANDIDATES_POLICY_ID)
-export NATIVE_TOKEN_POLICY_ID=$(cat /shared/NATIVE_TOKEN_POLICY_ID)
-export NATIVE_TOKEN_ASSET_NAME=$(cat /shared/NATIVE_TOKEN_ASSET_NAME)
-export ILLIQUID_SUPPLY_VALIDATOR_ADDRESS=$(cat /shared/ILLIQUID_SUPPLY_VALIDATOR_ADDRESS)
-
 # Check ACCOUNT and execute the corresponding command
 case "$ACCOUNT" in
   alice)
@@ -20,6 +11,13 @@ case "$ACCOUNT" in
             sleep 10
         fi
     done
+    export MC__FIRST_EPOCH_TIMESTAMP_MILLIS=$(cat /shared/MC__FIRST_EPOCH_TIMESTAMP_MILLIS)
+    export COMMITTEE_CANDIDATE_ADDRESS=$(cat /shared/COMMITTEE_CANDIDATE_ADDRESS)
+    export D_PARAMETER_POLICY_ID=$(cat /shared/D_PARAMETER_POLICY_ID)
+    export PERMISSIONED_CANDIDATES_POLICY_ID=$(cat /shared/PERMISSIONED_CANDIDATES_POLICY_ID)
+    export NATIVE_TOKEN_POLICY_ID=$(cat /shared/NATIVE_TOKEN_POLICY_ID)
+    export NATIVE_TOKEN_ASSET_NAME=$(cat /shared/NATIVE_TOKEN_ASSET_NAME)
+    export ILLIQUID_SUPPLY_VALIDATOR_ADDRESS=$(cat /shared/ILLIQUID_SUPPLY_VALIDATOR_ADDRESS)
     echo "pc-contracts-cli configuration complete. Starting node..."
     /usr/local/bin/partner-chains-node \
       --alice \
@@ -35,6 +33,13 @@ case "$ACCOUNT" in
       --blocks-pruning=archive &
     ;;
   bob)
+    export MC__FIRST_EPOCH_TIMESTAMP_MILLIS="1732098780000"
+    export COMMITTEE_CANDIDATE_ADDRESS="COMMITTEE_CANDIDATE_ADDRESS=addr_test1wre9jl2m00fwdsj5p4tdu37rcnm6eufce8lsvmpjl2ep2qqj92rxy"
+    export D_PARAMETER_POLICY_ID="D_PARAMETER_POLICY_ID=dd7628ddd457d57ce48ac8e3046bef4b5d2fb650bc2366812465deb2"
+    export PERMISSIONED_CANDIDATES_POLICY_ID="PERMISSIONED_CANDIDATES_POLICY_ID=15e98f9aeaecf4c0baf2fedac76b3a0f51d7e87f25dfffe723184592"
+    export NATIVE_TOKEN_POLICY_ID="ada83ddd029614381f00e28de0922ab0dec6983ea9dd29ae20eef9b4"
+    export NATIVE_TOKEN_ASSET_NAME="5043546f6b656e44656d6f"
+    export ILLIQUID_SUPPLY_VALIDATOR_ADDRESS="addr_test1wrhvtvx3f0g9wv9rx8kfqc60jva3e07nqujk2cspekv4mqs9rjdvz"
     echo "Starting node..."
     /usr/local/bin/partner-chains-node \
       --bob \
@@ -51,6 +56,13 @@ case "$ACCOUNT" in
       --blocks-pruning=archive &
     ;;
   charlie)
+    export MC__FIRST_EPOCH_TIMESTAMP_MILLIS="1732098780000"
+    export COMMITTEE_CANDIDATE_ADDRESS="COMMITTEE_CANDIDATE_ADDRESS=addr_test1wre9jl2m00fwdsj5p4tdu37rcnm6eufce8lsvmpjl2ep2qqj92rxy"
+    export D_PARAMETER_POLICY_ID="D_PARAMETER_POLICY_ID=dd7628ddd457d57ce48ac8e3046bef4b5d2fb650bc2366812465deb2"
+    export PERMISSIONED_CANDIDATES_POLICY_ID="PERMISSIONED_CANDIDATES_POLICY_ID=15e98f9aeaecf4c0baf2fedac76b3a0f51d7e87f25dfffe723184592"
+    export NATIVE_TOKEN_POLICY_ID="ada83ddd029614381f00e28de0922ab0dec6983ea9dd29ae20eef9b4"
+    export NATIVE_TOKEN_ASSET_NAME="5043546f6b656e44656d6f"
+    export ILLIQUID_SUPPLY_VALIDATOR_ADDRESS="addr_test1wrhvtvx3f0g9wv9rx8kfqc60jva3e07nqujk2cspekv4mqs9rjdvz"
     echo "Starting node..."
     /usr/local/bin/partner-chains-node \
       --charlie \
@@ -67,6 +79,13 @@ case "$ACCOUNT" in
       --blocks-pruning=archive &
     ;;
   dave)
+    export MC__FIRST_EPOCH_TIMESTAMP_MILLIS="1732098780000"
+    export COMMITTEE_CANDIDATE_ADDRESS="COMMITTEE_CANDIDATE_ADDRESS=addr_test1wre9jl2m00fwdsj5p4tdu37rcnm6eufce8lsvmpjl2ep2qqj92rxy"
+    export D_PARAMETER_POLICY_ID="D_PARAMETER_POLICY_ID=dd7628ddd457d57ce48ac8e3046bef4b5d2fb650bc2366812465deb2"
+    export PERMISSIONED_CANDIDATES_POLICY_ID="PERMISSIONED_CANDIDATES_POLICY_ID=15e98f9aeaecf4c0baf2fedac76b3a0f51d7e87f25dfffe723184592"
+    export NATIVE_TOKEN_POLICY_ID="ada83ddd029614381f00e28de0922ab0dec6983ea9dd29ae20eef9b4"
+    export NATIVE_TOKEN_ASSET_NAME="5043546f6b656e44656d6f"
+    export ILLIQUID_SUPPLY_VALIDATOR_ADDRESS="addr_test1wrhvtvx3f0g9wv9rx8kfqc60jva3e07nqujk2cspekv4mqs9rjdvz"
     echo "Starting node..."
     /usr/local/bin/partner-chains-node \
       --chain=/chain-spec.json \
@@ -84,6 +103,13 @@ case "$ACCOUNT" in
       --blocks-pruning=archive &
     ;;
   eve)
+    export MC__FIRST_EPOCH_TIMESTAMP_MILLIS="1732098780000"
+    export COMMITTEE_CANDIDATE_ADDRESS="COMMITTEE_CANDIDATE_ADDRESS=addr_test1wre9jl2m00fwdsj5p4tdu37rcnm6eufce8lsvmpjl2ep2qqj92rxy"
+    export D_PARAMETER_POLICY_ID="D_PARAMETER_POLICY_ID=dd7628ddd457d57ce48ac8e3046bef4b5d2fb650bc2366812465deb2"
+    export PERMISSIONED_CANDIDATES_POLICY_ID="PERMISSIONED_CANDIDATES_POLICY_ID=15e98f9aeaecf4c0baf2fedac76b3a0f51d7e87f25dfffe723184592"
+    export NATIVE_TOKEN_POLICY_ID="ada83ddd029614381f00e28de0922ab0dec6983ea9dd29ae20eef9b4"
+    export NATIVE_TOKEN_ASSET_NAME="5043546f6b656e44656d6f"
+    export ILLIQUID_SUPPLY_VALIDATOR_ADDRESS="addr_test1wrhvtvx3f0g9wv9rx8kfqc60jva3e07nqujk2cspekv4mqs9rjdvz"
     echo "Starting node..."
     /usr/local/bin/partner-chains-node \
       --chain=/chain-spec.json \
@@ -101,6 +127,13 @@ case "$ACCOUNT" in
       --blocks-pruning=archive &
     ;;
   ferdie)
+    export MC__FIRST_EPOCH_TIMESTAMP_MILLIS="1732098780000"
+    export COMMITTEE_CANDIDATE_ADDRESS="COMMITTEE_CANDIDATE_ADDRESS=addr_test1wre9jl2m00fwdsj5p4tdu37rcnm6eufce8lsvmpjl2ep2qqj92rxy"
+    export D_PARAMETER_POLICY_ID="D_PARAMETER_POLICY_ID=dd7628ddd457d57ce48ac8e3046bef4b5d2fb650bc2366812465deb2"
+    export PERMISSIONED_CANDIDATES_POLICY_ID="PERMISSIONED_CANDIDATES_POLICY_ID=15e98f9aeaecf4c0baf2fedac76b3a0f51d7e87f25dfffe723184592"
+    export NATIVE_TOKEN_POLICY_ID="ada83ddd029614381f00e28de0922ab0dec6983ea9dd29ae20eef9b4"
+    export NATIVE_TOKEN_ASSET_NAME="5043546f6b656e44656d6f"
+    export ILLIQUID_SUPPLY_VALIDATOR_ADDRESS="addr_test1wrhvtvx3f0g9wv9rx8kfqc60jva3e07nqujk2cspekv4mqs9rjdvz"
     echo "Starting node..."
     /usr/local/bin/partner-chains-node \
       --chain=/chain-spec.json \
