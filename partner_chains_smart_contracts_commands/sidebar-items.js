3ca23f1a64c8d910e4caeefaa6c0493200d313aa
AmbientTea <AmbientTea@users.noreply.github.com>
2024-11-21 13:58:46 +0000

    deploy: 2ee52c77624b86def4879e382357a80c06d23150

---
+++ partner_chains_smart_contracts_commands/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["SmartContractsCmd"],"mod":["init_governance"],"struct":["CommonArguments"]};
--- trait.impl/clap_builder/derive/trait.Subcommand.js
+++ trait.impl/clap_builder/derive/trait.Subcommand.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {
 "main_chain_follower_cli":[["impl Subcommand for <a class=\"enum\" href=\"main_chain_follower_cli/enum.Command.html\" title=\"enum main_chain_follower_cli::Command\">Command</a>"]],
 "partner_chains_cli":[["impl Subcommand for <a class=\"enum\" href=\"partner_chains_cli/enum.Command.html\" title=\"enum partner_chains_cli::Command\">Command</a>"]],
-"partner_chains_node_commands":[["impl Subcommand for <a class=\"enum\" href=\"partner_chains_node_commands/enum.PartnerChainsSubcommand.html\" title=\"enum partner_chains_node_commands::PartnerChainsSubcommand\">PartnerChainsSubcommand</a>"]]
+"partner_chains_node_commands":[["impl Subcommand for <a class=\"enum\" href=\"partner_chains_node_commands/enum.PartnerChainsSubcommand.html\" title=\"enum partner_chains_node_commands::PartnerChainsSubcommand\">PartnerChainsSubcommand</a>"]],
+"partner_chains_smart_contracts_commands":[["impl Subcommand for <a class=\"enum\" href=\"partner_chains_smart_contracts_commands/enum.SmartContractsCmd.html\" title=\"enum partner_chains_smart_contracts_commands::SmartContractsCmd\">SmartContractsCmd</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
