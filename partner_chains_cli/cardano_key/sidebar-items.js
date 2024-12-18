f5902b8d00089df9e64a2eddb2e82b395be7a9b9
kpinter-iohk <kpinter-iohk@users.noreply.github.com>
2024-12-18 13:27:18 +0000

    deploy: faa8d421128e82891f2ea108a60e934a438ee868

--- partner_chains_cli/cardano_key/sidebar-items.js
+++ partner_chains_cli/cardano_key/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["CBOR_KEY_PREFIX"],"fn":["get_key_bytes_from_file","get_mc_pkey_from_file"],"struct":["CardanoKeyFileContent"]};
+window.SIDEBAR_ITEMS = {"constant":["CBOR_KEY_PREFIX"],"fn":["get_key_bytes_from_file","get_mc_pkey_from_file","get_mc_pubkey_from_file"],"struct":["CardanoKeyFileContent"]};
