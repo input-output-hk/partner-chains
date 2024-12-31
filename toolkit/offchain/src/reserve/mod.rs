ab0048cd4c6b8a3f3106c985ffda701f19c26325
Lech Głowiak <lech.glowiak@iohk.io>
2024-12-31 07:09:12 +0100

    feat: implement building transaction for reserve creation

--- toolkit/offchain/src/reserve/mod.rs
+++ toolkit/offchain/src/reserve/mod.rs
@@ -1,3 +1,12 @@
 //! All smart-contracts related to Rewards Token Reserve Management
 
+use sidechain_domain::{AssetName, PolicyId};
+
+pub mod create;
 pub mod init;
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum ReserveToken {
+	Ada,
+	AssetId { policy_id: PolicyId, asset_name: AssetName },
+}
