06e873a72f50926b55c2908b6bac595f04f5c2c2
LGLO <LGLO@users.noreply.github.com>
2024-12-31 13:00:26 +0000

    deploy: 50646dca321ba2d94e0dd1e7b024a4d8e5afcbfb

--- trait.impl/sp_sidechain/trait.OnNewEpoch.js
+++ trait.impl/sp_sidechain/trait.OnNewEpoch.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"sidechain_runtime":[["impl <a class=\"trait\" href=\"sp_sidechain/trait.OnNewEpoch.html\" title=\"trait sp_sidechain::OnNewEpoch\">OnNewEpoch</a> for <a class=\"struct\" href=\"sidechain_runtime/struct.LogBeneficiaries.html\" title=\"struct sidechain_runtime::LogBeneficiaries\">LogBeneficiaries</a>"]],
+"sidechain_runtime":[["impl OnNewEpoch for <a class=\"struct\" href=\"sidechain_runtime/struct.LogBeneficiaries.html\" title=\"struct sidechain_runtime::LogBeneficiaries\">LogBeneficiaries</a>"]],
 "sp_sidechain":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
