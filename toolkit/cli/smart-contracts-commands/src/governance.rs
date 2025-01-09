938dfff72c23b4647be37779b5b87d9641c178f6
Nikolaos Dymitriadis <nikolaos.dymitriadis@iohk.io>
2025-01-09 10:23:22 +0100

    group the governance commands
    Signed-off-by: Nikolaos Dymitriadis <nikolaos.dymitriadis@iohk.io>

---
+++ toolkit/cli/smart-contracts-commands/src/governance.rs
@@ -0,0 +1,90 @@
+use crate::read_private_key_from_file;
+use jsonrpsee::http_client::HttpClient;
+use partner_chains_cardano_offchain::{
+	await_tx::FixedDelayRetries, init_governance::run_init_governance,
+	update_governance::run_update_governance,
+};
+use sidechain_domain::{MainchainAddressHash, UtxoId};
+
+#[derive(Clone, Debug, clap::Subcommand)]
+#[allow(clippy::large_enum_variant)]
+pub enum GovernanceCmd {
+	/// Initialize Partner Chain governance
+	Init(InitGovernanceCmd),
+	/// Update Partner Chain governance
+	Update(UpdateGovernanceCmd),
+}
+
+impl GovernanceCmd {
+	pub async fn execute(self) -> crate::CmdResult<()> {
+		match self {
+			Self::Init(cmd) => cmd.execute().await,
+			Self::Update(cmd) => cmd.execute().await,
+		}
+	}
+}
+
+#[derive(Clone, Debug, clap::Parser)]
+pub struct InitGovernanceCmd {
+	#[clap(flatten)]
+	common_arguments: crate::CommonArguments,
+	/// Governance authority hash to be set.
+	#[arg(long, short = 'g')]
+	governance_authority: MainchainAddressHash,
+	/// Path to the Cardano Payment Key file.
+	#[arg(long, short = 'k')]
+	payment_key_file: String,
+	/// Genesis UTXO of the new chain, it will be spent durning initialization. If not set, then one will be selected from UTXOs of the payment key.
+	#[arg(long, short = 'c')]
+	genesis_utxo: Option<UtxoId>,
+}
+
+impl InitGovernanceCmd {
+	pub async fn execute(self) -> crate::CmdResult<()> {
+		let payment_key = read_private_key_from_file(&self.payment_key_file)?;
+		let client = HttpClient::builder().build(self.common_arguments.ogmios_url)?;
+
+		run_init_governance(
+			self.governance_authority,
+			payment_key,
+			self.genesis_utxo,
+			&client,
+			FixedDelayRetries::two_minutes(),
+		)
+		.await?;
+		Ok(())
+	}
+}
+
+#[derive(Clone, Debug, clap::Parser)]
+pub struct UpdateGovernanceCmd {
+	#[clap(flatten)]
+	common_arguments: crate::CommonArguments,
+	/// Governance authority hash to be set.
+	#[arg(long, short = 'g')]
+	new_governance_authority: MainchainAddressHash,
+	/// Path to the Cardano Payment Key file.
+	#[arg(long, short = 'k')]
+	payment_key_file: String,
+	/// Genesis UTXO of the chain
+	#[arg(long, short = 'c')]
+	genesis_utxo: UtxoId,
+}
+
+impl UpdateGovernanceCmd {
+	pub async fn execute(self) -> crate::CmdResult<()> {
+		let payment_key = read_private_key_from_file(&self.payment_key_file)?;
+		let client = HttpClient::builder().build(self.common_arguments.ogmios_url)?;
+
+		run_update_governance(
+			self.new_governance_authority,
+			payment_key,
+			self.genesis_utxo,
+			&client,
+			FixedDelayRetries::two_minutes(),
+		)
+		.await?;
+
+		Ok(())
+	}
+}
--- toolkit/cli/smart-contracts-commands/src/lib.rs
+++ toolkit/cli/smart-contracts-commands/src/lib.rs
@@ -2,20 +2,15 @@ use sidechain_domain::*;
 
 pub mod d_parameter;
 pub mod get_scripts;
-pub mod init_governance;
+pub mod governance;
 pub mod register;
 pub mod reserve;
-pub mod update_governance;
 
 #[derive(Clone, Debug, clap::Subcommand)]
 #[allow(clippy::large_enum_variant)]
 pub enum SmartContractsCmd {
 	/// Print validator addresses and policy IDs of Partner Chain smart contracts
 	GetScripts(get_scripts::GetScripts),
-	/// Initialize Partner Chain governance
-	InitGovernance(init_governance::InitGovernanceCmd),
-	/// Update Partner Chain governance
-	UpdateGovernance(update_governance::UpdateGovernanceCmd),
 	/// Upsert DParameter
 	UpsertDParameter(d_parameter::UpsertDParameterCmd),
 	/// Register candidate
@@ -25,6 +20,9 @@ pub enum SmartContractsCmd {
 	/// Commands for management of rewards reserve
 	#[command(subcommand)]
 	Reserve(reserve::ReserveCmd),
+	/// Commands for management of on-chain governance
+	#[command(subcommand)]
+	Governance(governance::GovernanceCmd),
 }
 
 #[derive(Clone, Debug, clap::Parser)]
@@ -39,8 +37,7 @@ type CmdResult<T> = Result<T, Box<dyn std::error::Error + Send + Sync>>;
 impl SmartContractsCmd {
 	pub async fn execute(self) -> CmdResult<()> {
 		match self {
-			Self::InitGovernance(cmd) => cmd.execute().await,
-			Self::UpdateGovernance(cmd) => cmd.execute().await,
+			Self::Governance(cmd) => cmd.execute().await,
 			Self::GetScripts(cmd) => cmd.execute().await,
 			Self::UpsertDParameter(cmd) => cmd.execute().await,
 			Self::Register(cmd) => cmd.execute().await,
