ab4006a4c850ce86e3613bf3192548f0dd0e656e
Radosław Sporny <radoslaw.sporny@iohk.io>
2025-01-15 09:49:36 +0100

    chore: update dependencies on 1.3.0 local-env predefined setup for migration testing

--- dev/local-environment/setup.sh
+++ dev/local-environment/setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 PARTNER_CHAINS_NODE_IMAGE="ghcr.io/input-output-hk/partner-chains/partner-chains-node:v1.3.0"
-CARDANO_IMAGE="ghcr.io/intersectmbo/cardano-node:10.1.2"
-DBSYNC_IMAGE="ghcr.io/intersectmbo/cardano-db-sync:13.5.0.2"
-KUPO_IMAGE="cardanosolutions/kupo:v2.9.0"
-OGMIOS_IMAGE="cardanosolutions/ogmios:v6.9.0"
+CARDANO_IMAGE="ghcr.io/intersectmbo/cardano-node:10.1.4"
+DBSYNC_IMAGE="ghcr.io/intersectmbo/cardano-db-sync:13.6.0.4"
+KUPO_IMAGE="cardanosolutions/kupo:v2.10.0"
+OGMIOS_IMAGE="cardanosolutions/ogmios:v6.11.0"
 POSTGRES_IMAGE="postgres:15.3"
 SIDECHAIN_MAIN_CLI_IMAGE="node:22-bookworm"
 TESTS_IMAGE="python:3.10-slim"
