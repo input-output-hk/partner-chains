00a56d44470da0f5b7dd443f45c2ef9157e7da1f
kpinter-iohk <kpinter-iohk@users.noreply.github.com>
2025-01-27 11:22:00 +0000

    deploy: 79cfd5ae0cfdb843c6742c73a0c9fe2ecb20511c

--- trait.impl/sp_partner_chains_consensus_aura/inherent_digest/trait.InherentDigest.js
+++ trait.impl/sp_partner_chains_consensus_aura/inherent_digest/trait.InherentDigest.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"sidechain_mc_hash":[["impl <a class=\"trait\" href=\"sp_partner_chains_consensus_aura/inherent_digest/trait.InherentDigest.html\" title=\"trait sp_partner_chains_consensus_aura::inherent_digest::InherentDigest\">InherentDigest</a> for <a class=\"struct\" href=\"sidechain_mc_hash/struct.McHashInherentDigest.html\" title=\"struct sidechain_mc_hash::McHashInherentDigest\">McHashInherentDigest</a>"]],
+"sidechain_mc_hash":[["impl InherentDigest for <a class=\"struct\" href=\"sidechain_mc_hash/struct.McHashInherentDigest.html\" title=\"struct sidechain_mc_hash::McHashInherentDigest\">McHashInherentDigest</a>"]],
 "sp_partner_chains_consensus_aura":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
