a7ebf8670870bff5bdd1ac63ed14cfe9ee094b7a
Radosław Sporny <radoslaw.sporny@iohk.io>
2025-02-03 14:04:08 +0100

    ci: rename E2E-tests to e2e-tests

--- E2E-tests/src/blockchain_types.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-from .substrate_api import SubstrateApi
-from .pc_evm_api import PartnerChainEvmApi
-
-
-class BlockchainTypes(Enum):
-    substrate = SubstrateApi
-    midnight = SubstrateApi
-    pc_evm = PartnerChainEvmApi
