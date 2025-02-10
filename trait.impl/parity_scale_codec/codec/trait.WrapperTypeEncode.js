24cc8bac4057b6eda9b7cec333d25459784ebba7
AmbientTea <AmbientTea@users.noreply.github.com>
2025-02-10 15:47:12 +0000

    deploy: aeddf57ba87b3f19888ab7f46bf17b6e454b4c3c

---
+++ trait.impl/parity_scale_codec/codec/trait.WrapperTypeEncode.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"sidechain_domain":[["impl WrapperTypeEncode for <a class=\"struct\" href=\"sidechain_domain/struct.MainchainSignature.html\" title=\"struct sidechain_domain::MainchainSignature\">MainchainSignature</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
