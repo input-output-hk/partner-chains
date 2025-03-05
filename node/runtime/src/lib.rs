192887705ec3e4682c4c9cae81fc1e2206045900
Nikolaos Dymitriadis <nikolaos.dymitriadis@iohk.io>
2025-03-05 12:20:33 +0100

    introduce helper
    Signed-off-by: Nikolaos Dymitriadis <nikolaos.dymitriadis@iohk.io>

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -951,11 +951,8 @@ impl_runtime_apis! {
 
 	impl sp_block_production_log::BlockProductionLogApi<Block, CommitteeMember<CrossChainPublic, SessionKeys>>  for Runtime {
 		fn get_author(slot: Slot) -> CommitteeMember<CrossChainPublic, SessionKeys> {
-			let committee = SessionCommitteeManagement::get_current_committee().1;
-			// this implementation is compatible with round-robin employed by Aura
-			committee.get(*slot as usize % committee.len())
+			 SessionCommitteeManagement::get_current_authority_round_robin(*slot as usize)
 				.expect("Slot modulo committee size is a valid index unless committee is missing")
-				.clone()
 		}
 	}
 
