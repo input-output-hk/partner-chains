6c18056e2836ba0f2c264224705cea40c699aa9d
Nikolaos Dymitriadis <nndymitriadis@gmail.com>
2025-03-17 15:41:31 +0100

    change: Cleanup cli commands crate (#577)

--- toolkit/partner-chains-cli/src/register/mod.rs
+++ toolkit/partner-chains-cli/src/register/mod.rs
@@ -1,3 +1,60 @@
+use plutus_datum_derive::ToDatum;
+use secp256k1::PublicKey;
+use sidechain_domain::*;
+use std::{convert::Infallible, fmt::Display, str::FromStr};
+
 pub mod register1;
 pub mod register2;
 pub mod register3;
+
+#[derive(Clone, Debug, ToDatum)]
+pub struct RegisterValidatorMessage {
+	pub genesis_utxo: UtxoId,
+	pub sidechain_pub_key: SidechainPublicKey,
+	pub registration_utxo: UtxoId,
+}
+
+#[derive(Clone, Debug)]
+pub struct SidechainPublicKeyParam(pub SidechainPublicKey);
+
+impl Display for SidechainPublicKeyParam {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		write!(f, "0x{}", hex::encode(&self.0 .0))
+	}
+}
+
+impl FromStr for SidechainPublicKeyParam {
+	type Err = secp256k1::Error;
+
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		let trimmed = s.trim_start_matches("0x");
+		let pk = PublicKey::from_str(trimmed)?;
+		Ok(SidechainPublicKeyParam(SidechainPublicKey(pk.serialize().to_vec())))
+	}
+}
+
+#[derive(Clone, Debug)]
+pub struct PlainPublicKeyParam(pub String);
+
+impl Display for PlainPublicKeyParam {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		write!(f, "{}", self.0)
+	}
+}
+
+impl FromStr for PlainPublicKeyParam {
+	type Err = Infallible;
+
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		Ok(PlainPublicKeyParam(s.to_string()))
+	}
+}
+
+#[derive(Clone, Debug)]
+pub struct StakePoolSigningKeyParam(pub ed25519_zebra::SigningKey);
+
+impl From<[u8; 32]> for StakePoolSigningKeyParam {
+	fn from(key: [u8; 32]) -> Self {
+		Self(ed25519_zebra::SigningKey::from(key))
+	}
+}
--- toolkit/primitives/domain/src/crypto.rs
+++ toolkit/primitives/domain/src/crypto.rs
@@ -6,3 +6,42 @@ pub fn blake2b<const N: usize>(data: &[u8]) -> [u8; N] {
 		.try_into()
 		.unwrap_or_else(|_| panic!("hash output always has expected length of {N}"))
 }
+
+#[cfg(feature = "std")]
+pub use full_crypto::*;
+
+#[cfg(feature = "std")]
+mod full_crypto {
+	use crate::*;
+	use plutus::to_datum_cbor_bytes;
+	use plutus::ToDatum;
+	use secp256k1::{ecdsa::Signature, Message, PublicKey, SecretKey};
+
+	pub fn hash<T: ToDatum>(msg: T) -> [u8; 32] {
+		blake2b(to_datum_cbor_bytes(msg).as_slice())
+	}
+
+	pub fn sc_public_key_and_signature(key: SecretKey, hashed: [u8; 32]) -> (PublicKey, Signature) {
+		let public_key = PublicKey::from_secret_key_global(&key);
+		let signature = key.sign_ecdsa(Message::from_digest_slice(hashed.as_slice()).unwrap());
+		(public_key, signature)
+	}
+
+	pub fn sc_public_key_and_signature_for_datum<T: ToDatum>(
+		key: SecretKey,
+		datum_msg: T,
+	) -> (PublicKey, Signature) {
+		let hashed_msg = hash(datum_msg);
+		sc_public_key_and_signature(key, hashed_msg)
+	}
+
+	pub fn cardano_spo_public_key_and_signature<T: ToDatum>(
+		key: ed25519_zebra::SigningKey,
+		datum_msg: T,
+	) -> (StakePoolPublicKey, MainchainSignature) {
+		let message = to_datum_cbor_bytes(datum_msg);
+		let signature = MainchainSignature(key.sign(&message).into());
+		let public = StakePoolPublicKey(ed25519_zebra::VerificationKey::from(&key).into());
+		(public, signature)
+	}
+}
