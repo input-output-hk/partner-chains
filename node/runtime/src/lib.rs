760147aec0d24b15b1de128d8632357c850d26c5
Nikolaos Dymitriadis <nikolaos.dymitriadis@iohk.io>
2025-03-19 07:01:07 +0100

    introduce a sized string type
    Signed-off-by: Nikolaos Dymitriadis <nikolaos.dymitriadis@iohk.io>

--- node/runtime/src/lib.rs
+++ node/runtime/src/lib.rs
@@ -33,7 +33,7 @@ use parity_scale_codec::{Decode, Encode};
 use scale_info::TypeInfo;
 use serde::Deserialize;
 use session_manager::ValidatorManagementSessionManager;
-use sidechain_domain::byte_string::SizedByteString;
+use sidechain_domain::byte_string::{SizedByteString, SizedString};
 use sidechain_domain::{
 	DelegatorKey, MainchainKeyHash, PermissionedCandidateData, RegistrationData, ScEpochNumber,
 	ScSlotNumber, StakeDelegation, StakePoolPublicKey, UtxoId,
@@ -463,22 +463,9 @@ impl AsCardanoSPO for BlockAuthor {
 
 pub const MAX_METADATA_URL_LENGTH: u32 = 512;
 
-#[derive(Clone, Debug, MaxEncodedLen, Encode)]
-pub struct MetadataUrl(BoundedVec<u8, ConstU32<MAX_METADATA_URL_LENGTH>>);
-impl<'a> Deserialize<'a> for MetadataUrl {
-	fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-	where
-		D: serde::Deserializer<'a>,
-	{
-		Ok(Self(BoundedVec::truncate_from(
-			alloc::string::String::deserialize(deserializer)?.as_bytes().to_vec(),
-		)))
-	}
-}
-
 #[derive(Clone, Debug, MaxEncodedLen, Encode, Deserialize)]
 pub struct BlockProducerMetadata {
-	pub url: MetadataUrl,
+	pub url: SizedString<MAX_METADATA_URL_LENGTH>,
 	pub hash: SizedByteString<32>,
 }
 
--- toolkit/primitives/domain/src/byte_string.rs
+++ toolkit/primitives/domain/src/byte_string.rs
@@ -1,7 +1,13 @@
+use core::fmt::{Debug, Display};
+
 use alloc::vec::Vec;
 use byte_string_derive::byte_string;
 use parity_scale_codec::{Decode, Encode, MaxEncodedLen};
 use scale_info::TypeInfo;
+use serde::de::Error;
+use serde::Deserialize;
+use sp_core::bounded::BoundedVec;
+use sp_core::ConstU32;
 
 /// Wrapper for bytes that is serialized as hex string
 /// To be used for binary data that we want to display nicely but
@@ -43,5 +49,45 @@ impl<const N: usize> TryFrom<Vec<u8>> for SizedByteString<N> {
 impl<const N: usize> Default for SizedByteString<N> {
 	fn default() -> Self {
 		Self([0; N])
+	}
+}
+
+/// Byte-encoded text string with c
+#[derive(Eq, Clone, PartialEq, TypeInfo, Default, Encode, Decode, MaxEncodedLen)]
+pub struct SizedString<const N: u32>(pub BoundedVec<u8, ConstU32<N>>);
+
+impl<const N: u32> TryFrom<Vec<u8>> for SizedString<N> {
+	type Error = <BoundedVec<u8, ConstU32<N>> as TryFrom<Vec<u8>>>::Error;
+
+	fn try_from(value: Vec<u8>) -> Result<Self, Self::Error> {
+		Ok(Self(value.try_into()?))
+	}
+}
+
+impl<'a, const N: u32> Deserialize<'a> for SizedString<N> {
+	fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+	where
+		D: serde::Deserializer<'a>,
+	{
+		Ok(Self(
+			BoundedVec::try_from(
+				alloc::string::String::deserialize(deserializer)?.as_bytes().to_vec(),
+			)
+			.map_err(|_| D::Error::custom("Size limit exceeded"))?,
+		))
+	}
+}
+
+impl<const N: u32> Display for SizedString<N> {
+	fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+		f.write_str(
+			&alloc::string::String::from_utf8(self.0.to_vec()).map_err(|_| core::fmt::Error)?,
+		)
+	}
+}
+
+impl<const N: u32> Debug for SizedString<N> {
+	fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+		f.write_str(&alloc::format!("SizedString<{N}>({self:?})"))
 	}
 }
