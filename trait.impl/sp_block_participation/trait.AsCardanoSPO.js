6aad88a4071acd3052ad8331d2b64622d5305e48
LGLO <LGLO@users.noreply.github.com>
2025-04-01 18:43:48 +0000

    deploy: 5bfce60d3f42176edc68554d62df9859503d36d5

--- trait.impl/sp_block_participation/trait.AsCardanoSPO.js
+++ trait.impl/sp_block_participation/trait.AsCardanoSPO.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"partner_chains_demo_runtime":[["impl AsCardanoSPO for <a class=\"enum\" href=\"partner_chains_demo_runtime/enum.BlockAuthor.html\" title=\"enum partner_chains_demo_runtime::BlockAuthor\">BlockAuthor</a>"]],
+"partner_chains_demo_runtime":[["impl <a class=\"trait\" href=\"sp_block_participation/trait.AsCardanoSPO.html\" title=\"trait sp_block_participation::AsCardanoSPO\">AsCardanoSPO</a> for <a class=\"enum\" href=\"partner_chains_demo_runtime/enum.BlockAuthor.html\" title=\"enum partner_chains_demo_runtime::BlockAuthor\">BlockAuthor</a>"]],
 "sp_block_participation":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
