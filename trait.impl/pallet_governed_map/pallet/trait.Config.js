9ab35b1487d1e31c263bac74447a521e9244ac07
AmbientTea <AmbientTea@users.noreply.github.com>
2025-04-14 15:36:49 +0000

    deploy: a1d50d98692834e920aa2e54a0d367115cbac195

---
+++ trait.impl/pallet_governed_map/pallet/trait.Config.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["partner_chains_demo_runtime",[["impl Config for <a class=\"struct\" href=\"partner_chains_demo_runtime/struct.Runtime.html\" title=\"struct partner_chains_demo_runtime::Runtime\">Runtime</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[196]}
