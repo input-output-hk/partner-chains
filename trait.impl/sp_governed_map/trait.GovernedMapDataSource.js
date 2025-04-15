8b90a98d04f6ae4241f32c4320aca66d21c727af
kpinter-iohk <kpinter-iohk@users.noreply.github.com>
2025-04-15 10:36:34 +0000

    deploy: 3959559f8bfc14a18377cdaf09143bec2f22aff4

---
+++ trait.impl/sp_governed_map/trait.GovernedMapDataSource.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["partner_chains_db_sync_data_sources",[["impl GovernedMapDataSource for <a class=\"struct\" href=\"partner_chains_db_sync_data_sources/governed_map/struct.GovernedMapDataSourceImpl.html\" title=\"struct partner_chains_db_sync_data_sources::governed_map::GovernedMapDataSourceImpl\">GovernedMapDataSourceImpl</a>"]]],["partner_chains_mock_data_sources",[["impl GovernedMapDataSource for <a class=\"struct\" href=\"partner_chains_mock_data_sources/governed_map/struct.GovernedMapDataSourceMock.html\" title=\"struct partner_chains_mock_data_sources::governed_map::GovernedMapDataSourceMock\">GovernedMapDataSourceMock</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[316,308]}
