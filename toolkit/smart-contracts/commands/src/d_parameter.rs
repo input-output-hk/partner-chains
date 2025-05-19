bb1e9d510baefc029251dfcfce2e4fe2978291f3
jankun4 <michaljankun@gmail.com>
2025-05-19 02:22:28 +0200

    add docs for smart-contract/commands crate

--- toolkit/smart-contracts/commands/src/d_parameter.rs
+++ toolkit/smart-contracts/commands/src/d_parameter.rs
@@ -3,20 +3,26 @@ use partner_chains_cardano_offchain::d_param::upsert_d_param;
 use sidechain_domain::DParameter;
 
 #[derive(Clone, Debug, clap::Parser)]
+/// Command for upserting the D-parameter on the main chain
 pub struct UpsertDParameterCmd {
 	#[clap(flatten)]
 	common_arguments: crate::CommonArguments,
 	#[arg(long)]
+	/// Number of permissioned candidates
 	permissioned_candidates_count: u16,
 	#[arg(long)]
+	/// Number of registered candidates
 	registered_candidates_count: u16,
 	#[clap(flatten)]
+	/// Path to the payment key file
 	payment_key_file: PaymentFilePath,
 	#[clap(flatten)]
+	/// Genesis UTXO
 	genesis_utxo: GenesisUtxo,
 }
 
 impl UpsertDParameterCmd {
+	/// Creates the D-parameter and upserts it on the main chain.
 	pub async fn execute(self) -> crate::SubCmdResult {
 		let payment_key = self.payment_key_file.read_key()?;
 		let d_param = DParameter {
--- toolkit/smart-contracts/commands/src/get_scripts.rs
+++ toolkit/smart-contracts/commands/src/get_scripts.rs
@@ -2,14 +2,17 @@ use crate::GenesisUtxo;
 use partner_chains_cardano_offchain::scripts_data::get_scripts_data_with_ogmios;
 
 #[derive(Clone, Debug, clap::Parser)]
+/// Command for getting the addresses and policy ids of the partner chain smart contracts parametrised with the given genesis utxo
 pub struct GetScripts {
 	#[clap(flatten)]
 	common_arguments: crate::CommonArguments,
 	#[clap(flatten)]
+	/// Genesis UTXO
 	genesis_utxo: GenesisUtxo,
 }
 
 impl GetScripts {
+	/// Gets the addresses and policy ids of the partner chain smart contracts parametrised with the given genesis utxo
 	pub async fn execute(self) -> crate::SubCmdResult {
 		let client = self.common_arguments.get_ogmios_client().await?;
 		let scripts_data = get_scripts_data_with_ogmios(self.genesis_utxo.into(), &client).await?;
