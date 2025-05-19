92a900dd30ab40751983b0ea4c4148323fe9ff9f
Nikolaos Dymitriadis <nikolaos.dymitriadis@iohk.io>
2025-05-19 16:59:38 +0200

    change: ETCM-9687 Fixes and documentation for util crates

--- toolkit/utils/db-sync-sqlx/src/lib.rs
+++ toolkit/utils/db-sync-sqlx/src/lib.rs
@@ -1,3 +1,16 @@
+//! helpers and primitive types for querying Cardano [Db-Sync] data using [sqlx]
+//!
+//! # About
+//!
+//! This crate is meant to provide help when writing queries for data produced by [Db-Sync],
+//! a Cardano blockchain indexer. Db-Sync keeps the indexed data in a Postgre database that
+//! can be queried using SQL. This crate defines primitive types that correspond to column
+//! types used in the [Db-Sync schema], along with some other helpers and casts to types
+//! defined in [sidechain_domain].
+//!
+//! [sqlx]: https://github.com/launchbadge/sqlx
+//! [Db-Sync]: https://github.com/IntersectMBO/cardano-db-sync
+//! [Db-Sync schema]: https://github.com/IntersectMBO/cardano-db-sync/blob/master/doc/schema.md
 use num_traits::ToPrimitive;
 use sidechain_domain::*;
 use sqlx::database::Database;
@@ -6,11 +19,17 @@ use sqlx::error::BoxDynError;
 use sqlx::postgres::PgTypeInfo;
 use sqlx::*;
 
-/// Generates sqlx implementations for an unsigned wrapper of types that are signed.
-/// We expect that values will have always 0 as the most significant bit.
-/// For example TxIndex is in range of [0, 2^15-1], it will be u16 in domain,
-/// but it requires encoding and decoding like i16.
-/// See txindex, word31 and word63 types in db-sync schema definition.
+/// Macro to handle numeric types that are non-negative but are stored by Db-Sync using
+/// signed SQL types.
+///
+/// This macro generates an unsigned numeric domain type and sqlx trait implementation for
+/// decoding it from signed data coming from Db-Sync database. It expects that values will
+/// always have 0 as the most significant bit.
+///
+/// For example because [TxIndex] is in range of \[0, 2^15-1\], it will be [u16] in domain,
+/// but it requires encoding and decoding as i16.
+///
+/// See `txindex`, `word31` `and` word63 types in Db-Sync schema definition.
 #[macro_export]
 macro_rules! sqlx_implementations_for_wrapper {
 	($WRAPPED:ty, $DBTYPE:expr, $NAME:ty, $DOMAIN:ty) => {
@@ -62,28 +81,42 @@ macro_rules! sqlx_implementations_for_wrapper {
 	};
 }
 
+/// Cardano block number
 #[derive(Debug, Copy, Ord, PartialOrd, Clone, PartialEq, Eq)]
 pub struct BlockNumber(pub u32);
 sqlx_implementations_for_wrapper!(i32, "INT4", BlockNumber, McBlockNumber);
 
+/// Cardano epoch number
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub struct EpochNumber(pub u32);
 sqlx_implementations_for_wrapper!(i32, "INT4", EpochNumber, McEpochNumber);
 
+/// Cardano slot number
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub struct SlotNumber(pub u64);
 sqlx_implementations_for_wrapper!(i64, "INT8", SlotNumber, McSlotNumber);
 
+/// Index of a Cardano transaction output
+///
+/// This type corresponds to the following SQL type defined by Db-Sync.
+/// ```sql
 /// CREATE DOMAIN txindex AS smallint CONSTRAINT txindex_check CHECK ((VALUE >= 0));
+/// ```
 #[derive(Debug, Clone, PartialEq)]
 pub struct TxIndex(pub u16);
 sqlx_implementations_for_wrapper!(i16, "INT2", TxIndex, UtxoIndex);
 
+/// Index of a Cardano transaction with its block
 #[derive(Debug, Clone, Copy, PartialOrd, Ord, PartialEq, Eq)]
 pub struct TxIndexInBlock(pub u32);
 sqlx_implementations_for_wrapper!(i32, "INT4", TxIndexInBlock, McTxIndexInBlock);
 
+/// Number of ADA expressed in Lovelace (1 million-th of ADA)
+///
+/// This type corresponds to the following SQL type defined by Db-Sync.
+/// ```sql
 /// CREATE DOMAIN int65type AS numeric (20, 0) CHECK (VALUE >= -18446744073709551615 AND VALUE <= 18446744073709551615);
+/// ```
 #[derive(Debug, Clone, PartialEq)]
 pub struct TxValue(pub i128);
 
@@ -101,7 +134,12 @@ impl<'r> Decode<'r, Postgres> for TxValue {
 	}
 }
 
+/// Number of ADA delegated by a Cardano delegator to a single SPO, expressed in Lovelace (1 million-th of ADA)
+///
+/// This type corresponds to the following SQL type defined by Db-Sync.
+/// ```sql
 /// CREATE DOMAIN "lovelace" AS numeric(20,0) CONSTRAINT flyway_needs_this CHECK (VALUE >= 0::numeric AND VALUE <= '18446744073709551615'::numeric);
+/// ```
 #[derive(Debug, Clone, PartialEq)]
 pub struct StakeDelegation(pub u64);
 
@@ -119,6 +157,7 @@ impl<'r> Decode<'r, Postgres> for StakeDelegation {
 	}
 }
 
+/// Cardano native asset name, typically UTF-8 encoding of a human-readable name
 #[derive(Debug, Clone, sqlx::FromRow, PartialEq)]
 pub struct AssetName(pub Vec<u8>);
 
@@ -128,6 +167,7 @@ impl From<sidechain_domain::AssetName> for AssetName {
 	}
 }
 
+/// Cardano minting policy ID. This value is obtained by hashing the Plutus script of the policy.
 #[derive(Debug, Clone, sqlx::FromRow, PartialEq)]
 pub struct PolicyId(pub Vec<u8>);
 
@@ -137,9 +177,12 @@ impl From<sidechain_domain::PolicyId> for PolicyId {
 	}
 }
 
+/// Full identifier of a Cardano native asset
 #[derive(Debug, Clone, sqlx::FromRow, PartialEq)]
 pub struct Asset {
+	/// Minting policy ID of the asset
 	pub policy_id: PolicyId,
+	/// Asset name
 	pub asset_name: AssetName,
 }
 
@@ -156,6 +199,11 @@ impl From<sidechain_domain::AssetId> for Asset {
 	}
 }
 
+/// Helper row type for querying just epoch number
+#[deprecated(
+	since = "1.8.0",
+	note = "Deprecated due to not being either a primitive type or a complete Db-Sync table row."
+)]
 #[derive(Debug, Copy, Clone, sqlx::FromRow, PartialEq)]
 pub struct EpochNumberRow(pub EpochNumber);
 
@@ -165,6 +213,7 @@ impl From<EpochNumberRow> for EpochNumber {
 	}
 }
 
+/// Cardano address in human-readable form. Either Base58 for Byron addresses and Bech32 for Shelley.
 #[derive(Debug, Clone, sqlx::FromRow, PartialEq)]
 pub struct Address(pub String);
 
@@ -174,5 +223,8 @@ impl From<MainchainAddress> for Address {
 	}
 }
 
+/// Cardano epoch nonce, ie. random 32 bytes generated by Cardano every epoch.
+///
+/// This value can be used as a tamper-proof randomness seed.
 #[derive(Debug, Clone, sqlx::FromRow, PartialEq)]
 pub struct MainchainEpochNonce(pub Vec<u8>);
