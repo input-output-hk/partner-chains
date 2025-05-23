aa2862189c94d1514052c9b5b3db3b0924074de8
jankun4 <michaljankun@gmail.com>
2025-05-23 11:38:39 +0200

    add docs for ogmios-client

--- toolkit/utils/ogmios-client/src/transactions.rs
+++ toolkit/utils/ogmios-client/src/transactions.rs
@@ -3,6 +3,7 @@
 use crate::{ByNameParamsBuilder, OgmiosClient, OgmiosClientError, types::OgmiosTx};
 use serde::Deserialize;
 
+/// Trait that defines the methods for evaluating and submitting transactions via Ogmios.
 pub trait Transactions {
 	/// Evaluates a transaction.
 	///
@@ -52,36 +53,55 @@ impl<T: OgmiosClient> Transactions for T {
 
 #[derive(Clone, Debug, Deserialize, PartialEq, Eq, Default)]
 #[serde(rename_all = "camelCase")]
+/// Represents the response from evaluating a transaction.
 pub struct OgmiosEvaluateTransactionResponse {
+	/// The smart contract index.
 	pub validator: OgmiosValidatorIndex,
+	/// The costs of smart contract execution.
 	pub budget: OgmiosBudget,
 }
 
 #[derive(Clone, Debug, Deserialize, PartialEq, Eq, Default)]
+/// Represents the smart contract index.
 pub struct OgmiosValidatorIndex {
+	/// The index of the smart contract.
 	pub index: u32,
+	/// The purpose of the smart contract. Values allowed are:
+	/// * spend
+	/// * mint
+	/// * publish
+	/// * withdraw
+	/// * vote
+	/// * propose
 	pub purpose: String,
 }
 
 impl OgmiosValidatorIndex {
+	/// Creates a new smart contract index.
 	pub fn new(index: u32, purpose: &str) -> Self {
 		Self { index, purpose: purpose.into() }
 	}
 }
 
 #[derive(Clone, Debug, Deserialize, PartialEq, Eq, Default)]
+/// Represents the budget for smart contract execution.
 pub struct OgmiosBudget {
+	/// The memory budget.
 	pub memory: u64,
+	/// The CPU budget.
 	pub cpu: u64,
 }
 
 impl OgmiosBudget {
+	/// Creates a new budget.
 	pub fn new(memory: u64, cpu: u64) -> Self {
 		Self { memory, cpu }
 	}
 }
 
 #[derive(Clone, Debug, Deserialize, PartialEq, Eq, Default)]
+/// Represents the response from submitting a transaction.
 pub struct SubmitTransactionResponse {
+	/// The transaction hash.
 	pub transaction: OgmiosTx,
 }
