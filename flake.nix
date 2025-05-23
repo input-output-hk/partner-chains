6f6f3c6a8a18b4510feda6baa3fd073ae71e3a78
Tobias Pflug <tobias.pflug@gmail.com>
2025-05-23 11:26:16 +0200

    Simplify nix setup to single flake.nix (#814)
    Reduces the nix setup to a single flake.nix containing the devShell
    definition that works for linux and darwin
    
    - Drops process-compose setup which has been deprecated by local-env
    - Drops packages which were only provided for process-compose setup
    - Drops flake-modules and uses flake-utils instead

--- flake.nix
+++ flake.nix
@@ -1,53 +1,101 @@
 {
+  description = "Your devShell environment using flake-utils";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    # Rust toolchains in nix
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # Nix helpers
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    flake-compat = {
-      url = "github:input-output-hk/flake-compat/fixes";
-      flake = false;
-    };
-    process-compose.url = "github:Platonic-Systems/process-compose-flake";
-    services-flake.url = "github:juspay/services-flake";
+    flake-utils.url = "github:numtide/flake-utils";
 
-    # Partner Chains deps
     cardano-node = {
       url = "github:IntersectMBO/cardano-node/10.1.4";
       flake = false;
     };
-    cardano-dbsync = {
-      url = "github:IntersectMBO/cardano-db-sync/13.6.0.4";
-      flake = false;
-    };
-    # ... ogmios is defined in dev/nix/packages
 
-    configurations = {
-      url = "github:input-output-hk/cardano-configurations";
+    flake-compat = {
+      url = "github:input-output-hk/flake-compat/fixes";
       flake = false;
     };
   };
-  outputs = inputs @ {
-    self,
-    nixpkgs,
-    flake-parts,
-    ...
-  }:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux" "aarch64-darwin"];
-      imports = [
-        inputs.process-compose.flakeModule
-        ./dev/nix/shell.nix
-        ./dev/nix/packages
-        ./dev/nix/processes.nix
-      ];
-    };
+
+  outputs =
+    {
+      nixpkgs,
+      fenix,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+        rustToolchain = fenix.packages.${system}.fromToolchainFile {
+          file = ./rust-toolchain.toml;
+          sha256 = "X/4ZBHO3iW0fOenQ3foEvscgAPJYl2abspaBThDOukI=";
+        };
+
+        isLinux = pkgs.stdenv.isLinux;
+        isDarwin = pkgs.stdenv.isDarwin;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages =
+            with pkgs;
+            [
+              awscli2
+              bashInteractive
+              cargo-edit
+              cargo-license
+              coreutils
+              docker-compose
+              earthly
+              gawk
+              gnumake
+              kubectl
+              libiconv
+              nixfmt-rfc-style
+              openssl
+              pkg-config
+              protobuf
+              python312
+              python312Packages.pip
+              python312Packages.virtualenv
+              rustToolchain
+              sops
+            ]
+            ++ (if isDarwin then [ pkgs.darwin.apple_sdk.frameworks.SystemConfiguration ] else [ pkgs.clang ]);
+
+          shellHook = ''
+            export RUST_SRC_PATH="${rustToolchain}/lib/rustlib/src/rust/library"
+            export LIBCLANG_PATH="${pkgs.libclang.lib}/lib"
+            export LD_LIBRARY_PATH="${
+              pkgs.lib.makeLibraryPath [
+                rustToolchain
+                pkgs.libz
+              ]
+            }"
+
+            export ROCKSDB_LIB_DIR="${pkgs.rocksdb}/lib/"
+            export OPENSSL_NO_VENDOR=1
+            export OPENSSL_DIR="${pkgs.openssl.dev}"
+            export OPENSSL_INCLUDE_DIR="${pkgs.openssl.dev}/include"
+            export OPENSSL_LIB_DIR="${pkgs.openssl.out}/lib"
+
+            export CRATE_CC_NO_DEFAULTS=1
+            ${if isLinux then "export CFLAGS=-DJEMALLOC_STRERROR_R_RETURNS_CHAR_WITH_GNU_SOURCE" else ""}
+          '';
+        };
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
+
   nixConfig = {
     allow-import-from-derivation = true;
     accept-flake-config = true;
