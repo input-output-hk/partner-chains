4f889a6f0ed17d0ccf8615d1c6286d8b384b8d4a
Tomasz Bartos <tomasz.bartos@iohk.io>
2025-05-26 13:00:50 +0200

    Revert "Not start from genesis"
    This reverts commit 9a793539b4f22b2246c48b4db1236a732f0321a7.

--- demo/node/src/template_chain_spec.rs
+++ demo/node/src/template_chain_spec.rs
@@ -1,8 +1,8 @@
 use crate::chain_spec::*;
 use partner_chains_demo_runtime::{
-	AuraConfig, BalancesConfig, BeefyConfig, GovernedMapConfig, GrandpaConfig,
-	NativeTokenManagementConfig, RuntimeGenesisConfig, SessionCommitteeManagementConfig,
-	SessionConfig, SidechainConfig, SudoConfig, SystemConfig, TestHelperPalletConfig,
+	AuraConfig, BalancesConfig, GovernedMapConfig, GrandpaConfig, NativeTokenManagementConfig,
+	RuntimeGenesisConfig, SessionCommitteeManagementConfig, SessionConfig, SidechainConfig,
+	SudoConfig, SystemConfig, TestHelperPalletConfig,
 };
 use sc_service::ChainType;
 
@@ -19,7 +19,7 @@ pub fn chain_spec() -> Result<ChainSpec, envy::Error> {
 			dev_accounts: None,
 		},
 		aura: AuraConfig { authorities: vec![] },
-		beefy: BeefyConfig { authorities: vec![], genesis_block: Some(42) },
+		beefy: Default::default(),
 		grandpa: GrandpaConfig { authorities: vec![], ..Default::default() },
 		sudo: SudoConfig {
 			// No sudo account by default, please update with your preferences.
