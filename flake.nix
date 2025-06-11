e4c87bad8841a6e2d6f2727965bc7626e13e918b
Skylar Simoncelli <128400943+skylar-simoncelli@users.noreply.github.com>
2025-06-11 19:42:32 +0100

    Replace earthly attempt 4 (#855)
    * minimal setup for testing
    
    minimal ci.yml
    
    * add to full setup
    
    * Replace use of earthly in dispatch
    
    * Remove redundant earthly files
    
    * update chain spec creation code
    
    * fix artifact upload path
    
    * fix: to-build logic + deny.toml
    
    * fix: unicode
    
    * fix: CDLA
    
    * fix: apache
    
    * fix: GPL
    
    * fix: GPL
    
    * fix: unused-allow
    
    * fix. allow
    
    * fix: allow
    
    * fix: order
    
    * fix: don't check licenses
    
    * fix: to-build and remove non linux x86_64 builds
    
    * fix: overwrite flake
    
    * fix: no to-build
    
    * fix: clean
    
    * fix: debug
    
    * fix: rm
    
    * fix. sha
    
    * fix: fetch depth
    
    * fix: remove devshell tests
    
    * fix: build and push
    
    * chore: remove unused files blabla and foobar
    
    * fix:: sha
    
    ---------
    
    Co-authored-by: tobias pflug <tobias.pflug@gmail.com>

--- flake.nix
+++ flake.nix
@@ -138,6 +138,7 @@
               libiconv
               nixfmt-rfc-style
               openssl
+              patchelf
               pkg-config
               protobuf
               python312
@@ -145,6 +146,7 @@
               python312Packages.virtualenv
               rustToolchain
               sops
+              xxd
             ]
             ++ (if isDarwin then [ pkgs.darwin.apple_sdk.frameworks.SystemConfiguration ] else [ pkgs.clang ]);
 
