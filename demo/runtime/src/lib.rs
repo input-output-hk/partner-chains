1ec4d08545772bb5f122a628e37e4d5a31caae61
Tomasz Bartos <tomasz.bartos@iohk.io>
2025-06-17 15:30:34 +0200

    Upgrade to latest master

--- demo/runtime/src/lib.rs
+++ demo/runtime/src/lib.rs
@@ -363,7 +363,7 @@ impl pallet_balances::Config for Runtime {
 	type WeightInfo = pallet_balances::weights::SubstrateWeight<Runtime>;
 	type FreezeIdentifier = ();
 	type MaxFreezes = ();
-	type RuntimeHoldReason = ();
+	type RuntimeHoldReason = RuntimeHoldReason;
 	type RuntimeFreezeReason = RuntimeFreezeReason;
 	type DoneSlashHandler = ();
 }
