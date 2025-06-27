f42a31d79150843128af097fbf1e3c9fcdb041ad
Lech Głowiak <lech.glowiak@iohk.io>
2025-06-27 11:24:38 +0200

    Extract runner base image to avoid rebuilds

--- dev/docker/build-images-local.sh
+++ dev/docker/build-images-local.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 # This script has to be executed from the root of the repository to enable proper context for the Docker build. `./dev/docker/build-images-local.sh`
 docker build --platform linux/amd64 --pull -f dev/docker/builder.Dockerfile -t "partner-chains-node-builder:latest" --ssh default . && \
+docker build --platform linux/amd64 --pull -f dev/docker/runner.Dockerfile -t "partner-chains-node-runner:latest" --ssh default . && \
 docker build --platform linux/amd64 -f dev/docker/Dockerfile -t partner-chains-node:latest dev/docker
