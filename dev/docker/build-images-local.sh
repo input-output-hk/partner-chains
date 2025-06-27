19fb10168cadebd137733f55c04367ece645115c
Lech Głowiak <lech.glowiak@iohk.io>
2025-06-27 10:26:55 +0200

    Use --platform linux/amd64 to not mix architectures on Mac

--- dev/docker/build-images-local.sh
+++ dev/docker/build-images-local.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 # This script has to be executed from the root of the repository to enable proper context for the Docker build. `./dev/docker/build-images-local.sh`
-docker build --pull -f dev/docker/builder.Dockerfile -t "partner-chains-node-builder:latest" --ssh default . && \
-docker build -f dev/docker/Dockerfile -t partner-chains-node:latest dev/docker
+docker build --platform linux/amd64 --pull -f dev/docker/builder.Dockerfile -t "partner-chains-node-builder:latest" --ssh default . && \
+docker build --platform linux/amd64 -f dev/docker/Dockerfile -t partner-chains-node:latest dev/docker
