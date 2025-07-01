15fc550812f007fe24dfd73b8a3342dfc1df4d61
skylar-simoncelli <skylar.simoncelli@iohk.io>
2025-07-01 16:29:02 +0100

    fix: update cardano-cli command to use 'shelley' for address building in entrypoint script

--- dev/local-environment-dynamic/configurations/cardano/entrypoint.sh
+++ dev/local-environment-dynamic/configurations/cardano/entrypoint.sh
@@ -178,12 +178,12 @@ for i in $(seq 1 $NUM_REGISTERED_NODES_TO_PROCESS); do
         continue
     fi
     
-    node_payment_address=$(cardano-cli address build \
+    node_payment_address=$(cardano-cli shelley address build \
         --payment-verification-key-file "${NODE_SPECIFIC_KEYS_DIR}/payment.vkey" \
         --stake-verification-key-file "${NODE_SPECIFIC_KEYS_DIR}/stake.vkey" \
         --testnet-magic 42)
     if [ -z "$node_payment_address" ]; then 
-        echo "Error building payment address for registered-$i!"
+        echo "[WARN] Error building payment address for registered-$i!"
         continue
     fi
     
