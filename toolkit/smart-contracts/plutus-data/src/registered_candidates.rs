41ce67f6fac021f8d2808f254759c8a8015a231f
Lech Głowiak <lech.glowiak@iohk.io>
2025-07-08 17:01:23 +0200

    feat: parse V1 of Registered Candidate Datum

--- toolkit/smart-contracts/plutus-data/src/registered_candidates.rs
+++ toolkit/smart-contracts/plutus-data/src/registered_candidates.rs
@@ -1,6 +1,7 @@
 //! Plutus data types for registered candidates.
 use crate::{
 	PlutusDataExtensions, VersionedDatum, VersionedDatumWithLegacy, VersionedGenericDatum,
+	candidate_keys::*,
 };
 use cardano_serialization_lib::*;
 use sidechain_domain::*;
@@ -56,6 +57,23 @@ pub enum RegisterValidatorDatum {
 		/// Registering SPO's GRANDPA public key
 		grandpa_pub_key: GrandpaPublicKey,
 	},
+	/// V1 datum with support for generic keys
+	V1 {
+		/// Stake ownership information of registered candidate.
+		stake_ownership: AdaBasedStaking,
+		/// Sidechain public key of the candidate. See [SidechainPublicKey] for more details.
+		sidechain_pub_key: SidechainPublicKey,
+		/// Sidechain key signature of the registration message.
+		sidechain_signature: SidechainSignature,
+		/// UTxO id that is a part of the signed registration message.
+		/// It is spent during the registration process. Prevents replay attacks.
+		registration_utxo: UtxoId,
+		/// Hash of the registering SPO's Cardano public key.
+		/// Used by offchain code to find the registration UTXO when re-registering or de-registering.
+		own_pkh: MainchainKeyHash,
+		/// Additional keys of the registered candidate, these are specific to the keys used by the partner chain
+		keys: Vec<KeyTypeIdAndBytes>,
+	},
 }
 
 impl TryFrom<PlutusData> for RegisterValidatorDatum {
@@ -80,6 +98,8 @@ impl VersionedDatumWithLegacy for RegisterValidatorDatum {
 	) -> Result<Self, String> {
 		match version {
 			0 => decode_v0_register_validator_datum(datum, appendix)
+				.ok_or("Can not parse appendix".to_string()),
+			1 => decode_v1_register_validator_datum(datum, appendix)
 				.ok_or("Can not parse appendix".to_string()),
 			_ => Err(format!("Unknown version: {version}")),
 		}
@@ -122,6 +142,22 @@ impl From<RegisterValidatorDatum> for CandidateRegistration {
 				aura_pub_key,
 				grandpa_pub_key,
 			},
+			RegisterValidatorDatum::V1 {
+				stake_ownership,
+				sidechain_pub_key,
+				sidechain_signature,
+				registration_utxo,
+				own_pkh,
+				keys,
+			} => CandidateRegistration {
+				stake_ownership,
+				partner_chain_pub_key: sidechain_pub_key,
+				partner_chain_signature: sidechain_signature,
+				registration_utxo,
+				own_pkh,
+				aura_pub_key: AuraPublicKey(find_or_empty(&keys, AURA_TYPE_ID)),
+				grandpa_pub_key: GrandpaPublicKey(find_or_empty(&keys, GRANDPA_TYPE_ID)),
+			},
 		}
 	}
 }
@@ -151,6 +187,33 @@ fn decode_v0_register_validator_datum(
 		own_pkh,
 		aura_pub_key,
 		grandpa_pub_key,
+	})
+}
+
+fn decode_v1_register_validator_datum(
+	datum: &PlutusData,
+	appendix: &PlutusData,
+) -> Option<RegisterValidatorDatum> {
+	let fields = appendix
+		.as_constr_plutus_data()
+		.filter(|datum| datum.alternative().is_zero())
+		.filter(|datum| datum.data().len() == 5)?
+		.data();
+
+	let stake_ownership = decode_ada_based_staking_datum(fields.get(0))?;
+	let sidechain_pub_key = SidechainPublicKey(decode_key_id_and_bytes(&fields.get(1))?.1);
+	let sidechain_signature = fields.get(2).as_bytes().map(SidechainSignature)?;
+	let registration_utxo = decode_utxo_id_datum(fields.get(3))?;
+	let keys = decode_key_id_and_bytes_list(&fields.get(4))?;
+
+	let own_pkh = MainchainKeyHash(datum.as_bytes()?.try_into().ok()?);
+	Some(RegisterValidatorDatum::V1 {
+		stake_ownership,
+		sidechain_pub_key,
+		sidechain_signature,
+		registration_utxo,
+		own_pkh,
+		keys,
 	})
 }
 
@@ -237,6 +300,29 @@ impl From<RegisterValidatorDatum> for PlutusData {
 				}
 				.into()
 			},
+			RegisterValidatorDatum::V1 {
+				stake_ownership,
+				sidechain_pub_key,
+				sidechain_signature,
+				registration_utxo,
+				own_pkh,
+				keys,
+			} => {
+				let mut appendix_fields = PlutusList::new();
+				appendix_fields.add(&stake_ownership_to_plutus_data(stake_ownership));
+				appendix_fields
+					.add(&key_id_and_bytes_to_plutus(CROSS_CHAIN_TYPE_ID, &sidechain_pub_key.0));
+				appendix_fields.add(&PlutusData::new_bytes(sidechain_signature.0));
+				appendix_fields.add(&utxo_id_to_plutus_data(registration_utxo));
+				appendix_fields.add(&key_id_and_bytes_list_to_plutus(&keys));
+				let appendix = ConstrPlutusData::new(&BigNum::zero(), &appendix_fields);
+				VersionedGenericDatum {
+					datum: PlutusData::new_bytes(own_pkh.0.to_vec()),
+					appendix: PlutusData::new_constr_plutus_data(&appendix),
+					version: 1,
+				}
+				.into()
+			},
 		}
 	}
 }
@@ -286,6 +372,34 @@ mod tests {
 		}
 	}
 
+	fn test_datum_v1() -> RegisterValidatorDatum {
+		RegisterValidatorDatum::V1 {
+			stake_ownership: AdaBasedStaking {
+				pub_key: StakePoolPublicKey(hex!("bfbee74ab533f40979101057f96de62e95233f2a5216eb16b54106f09fd7350d")),
+				signature: MainchainSignature(hex!("28d1c3b7df297a60d24a3f88bc53d7029a8af35e8dd876764fd9e7a24203a3482a98263cc8ba2ddc7dc8e7faea31c2e7bad1f00e28c43bc863503e3172dc6b0a").into()),
+			},
+			sidechain_pub_key: SidechainPublicKey(hex!("02fe8d1eb1bcb3432b1db5833ff5f2226d9cb5e65cee430558c18ed3a3c86ce1af").into()),
+			sidechain_signature: SidechainSignature(hex!("f8ec6c7f935d387aaa1693b3bf338cbb8f53013da8a5a234f9c488bacac01af259297e69aee0df27f553c0a1164df827d016125c16af93c99be2c19f36d2f66e").into()),
+			registration_utxo: UtxoId {
+				tx_hash: McTxHash(hex!("cdefe62b0a0016c2ccf8124d7dda71f6865283667850cc7b471f761d2bc1eb13")),
+				index: UtxoIndex(1),
+			},
+			own_pkh: MainchainKeyHash(hex!("aabbccddeeff00aabbccddeeff00aabbccddeeff00aabbccddeeff00")),
+			keys: vec![
+					(
+						*b"aura",
+						hex!("d43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d")
+							.into(),
+					),
+					(
+						*b"gran",
+						hex!("88dc3417d5058ec4b4503e0c12ea1a0a89be200fe98922423d4334014fa6b0ee")
+							.into(),
+					),
+				]
+		}
+	}
+
 	#[test]
 	fn valid_legacy_registration() {
 		let plutus_data = test_plutus_data!({
@@ -318,7 +432,7 @@ mod tests {
 		assert_eq!(RegisterValidatorDatum::try_from(plutus_data).unwrap(), test_datum_v0())
 	}
 
-	fn test_versioned_datum_json() -> serde_json::Value {
+	fn test_versioned_datum_v0_json() -> serde_json::Value {
 		serde_json::json!({
 			"list": [
 				{ "bytes": "aabbccddeeff00aabbccddeeff00aabbccddeeff00aabbccddeeff00" },
@@ -353,15 +467,64 @@ mod tests {
 		})
 	}
 
+	fn test_versioned_datum_v1_json() -> serde_json::Value {
+		serde_json::json!({
+			"list": [
+				{"bytes": "aabbccddeeff00aabbccddeeff00aabbccddeeff00aabbccddeeff00" },
+				{
+					"constructor": 0,
+					"fields": [
+						{
+							"constructor": 0,
+							"fields": [
+								{"bytes": "bfbee74ab533f40979101057f96de62e95233f2a5216eb16b54106f09fd7350d" },
+								{"bytes": "28d1c3b7df297a60d24a3f88bc53d7029a8af35e8dd876764fd9e7a24203a3482a98263cc8ba2ddc7dc8e7faea31c2e7bad1f00e28c43bc863503e3172dc6b0a" }
+							]
+						},
+						{"list": [{"bytes": "63726368"}, { "bytes": "02fe8d1eb1bcb3432b1db5833ff5f2226d9cb5e65cee430558c18ed3a3c86ce1af" }]},
+						{"bytes": "f8ec6c7f935d387aaa1693b3bf338cbb8f53013da8a5a234f9c488bacac01af259297e69aee0df27f553c0a1164df827d016125c16af93c99be2c19f36d2f66e" },
+						{
+							"fields": [
+								{
+									"constructor": 0,
+									"fields": [ {"bytes": "cdefe62b0a0016c2ccf8124d7dda71f6865283667850cc7b471f761d2bc1eb13"} ]
+								},
+								{"int": 1 }
+							],
+							"constructor": 0
+						},
+						{"list": [
+							{"list": [{"bytes": "61757261"}, {"bytes": "d43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d" }]},
+							{"list": [{"bytes": "6772616e"}, {"bytes": "88dc3417d5058ec4b4503e0c12ea1a0a89be200fe98922423d4334014fa6b0ee" }]}
+						]}
+					]
+				},
+				{"int": 1 }
+			]
+		})
+	}
+
 	#[test]
 	fn valid_v0_registration_from_plutus_data() {
-		let plutus_data = json_to_plutus_data(test_versioned_datum_json());
+		let plutus_data = json_to_plutus_data(test_versioned_datum_v0_json());
 		assert_eq!(RegisterValidatorDatum::try_from(plutus_data).unwrap(), test_datum_v0())
+	}
+
+	#[test]
+	fn valid_v1_registration_from_plutus_data() {
+		let plutus_data = json_to_plutus_data(test_versioned_datum_v1_json());
+		assert_eq!(RegisterValidatorDatum::try_from(plutus_data).unwrap(), test_datum_v1())
+	}
+
+	#[test]
+	fn valid_v1_registration_to_plutus_data() {
+		let plutus_data: PlutusData = test_datum_v1().into();
+		assert_eq!(plutus_data_to_json(plutus_data), test_versioned_datum_v1_json())
 	}
 
 	#[test]
 	fn v0_registration_to_plutus_data() {
 		let plutus_data: PlutusData = test_datum_v0().into();
-		assert_eq!(plutus_data_to_json(plutus_data), test_versioned_datum_json())
+		assert_eq!(plutus_data_to_json(plutus_data), test_versioned_datum_v0_json())
 	}
 }
