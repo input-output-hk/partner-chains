88a5394e9abddb66a2dad38569fb9b6339120b9d
Lech Głowiak <lech.glowiak@iohk.io>
2025-07-16 12:52:07 +0200

    base changes from Lech

--- toolkit/partner-chains-cli/src/runtime_bindings.rs
+++ toolkit/partner-chains-cli/src/runtime_bindings.rs
@@ -1,6 +1,12 @@
 use crate::{CreateChainSpecConfig, keystore::KeyDefinition};
 use authority_selection_inherents::MaybeFromCandidateKeys;
 
+/// Trait wrapping Substrate runtime type. Should be implemented for the runtime of the node.
+pub trait RuntimeTypeWrapper {
+	/// Substrate runtime type.
+	type Runtime;
+}
+
 /// Trait defining Partner Chain governance related types.
 pub trait PartnerChainRuntime {
 	/// User keys type
