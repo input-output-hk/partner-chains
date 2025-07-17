34dc3ad92eb6754e88bcfe9d94d6f204444e4bfb
Krisztian Pinter <krisztian.pinter@iohk.io>
2025-07-17 16:52:05 +0700

    spend existing ics utxo

--- toolkit/smart-contracts/offchain/src/reserve/mod.rs
+++ toolkit/smart-contracts/offchain/src/reserve/mod.rs
@@ -113,6 +113,28 @@ impl ReserveData {
 
 		Ok(ReserveUtxo { utxo: reserve_utxo, datum: reserve_settings })
 	}
+
+	pub(crate) async fn get_ics_utxos<T: QueryLedgerState>(
+		&self,
+		ctx: &TransactionContext,
+		client: &T,
+	) -> Result<Vec<OgmiosUtxo>, anyhow::Error> {
+		let ics_address = self.scripts.illiquid_circulation_supply_validator.address(ctx.network).to_bech32(None)?;
+		let ics_utxos = client.query_utxos(&[ics_address]).await?;
+
+		let auth_token_asset_id = AssetId {
+			policy_id: self.scripts.auth_policy.policy_id(),
+			asset_name: AssetName::empty(),
+		};
+
+		let reserve_utxo = ics_utxos
+			.into_iter()
+			.filter(|utxo| {
+				utxo.get_asset_amount(&auth_token_asset_id) == 1u64
+			}).collect();
+
+		Ok(reserve_utxo)
+	}
 }
 
 pub(crate) struct TokenAmount {
