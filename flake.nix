66db0f1cc60bddce69e282959fbdd9a8462db3eb
Lech Głowiak <LGLO@users.noreply.github.com>
2025-07-28 09:45:23 +0200

    chore: update Rust to 1.88 and use wasm32v1-none

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
         };
         rustToolchain = fenix.packages.${system}.fromToolchainFile {
           file = ./rust-toolchain.toml;
-          sha256 = "X/4ZBHO3iW0fOenQ3foEvscgAPJYl2abspaBThDOukI=";
+          sha256 = "Qxt8XAuaUR2OMdKbN4u8dBJOhSHxS+uS06Wl9+flVEk=";
         };
 
         isLinux = pkgs.stdenv.isLinux;
