ac934667ad50cefe83bfaa6beb8e6cf59c6a6633
Nikolaos Dymitriadis <nikolaos.dymitriadis@iohk.io>
2025-08-07 18:28:45 +0200

    wip

--- toolkit/data-sources/db-sync/src/governed_map/tests.rs
+++ toolkit/data-sources/db-sync/src/governed_map/tests.rs
@@ -26,105 +26,120 @@ const BLOCK_6: McBlockHash =
 const BLOCK_7: McBlockHash =
 	McBlockHash(hex!("b702000000000000000000000000000000000000000000000000000000000008"));
 
-#[sqlx::test(migrations = "./testdata/governed-map/migrations-v2")]
-async fn test_governed_map_fails_on_wrong_block_hash(pool: PgPool) {
-	let source = make_source(pool, TxInConfiguration::Consumed);
-	let mc_block =
-		McBlockHash(hex!("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"));
-	let result = source.get_mapping_changes(None, mc_block, scripts()).await;
-	assert_err!(result);
+macro_rules! with_migration_versions {
+	($(async fn $name:ident($pool:ident: PgPool, $tx_in_cfg:ident: TxInConfiguration) $body:block )*) => {
+
+		$(
+            paste::paste!(
+				async fn $name($pool: PgPool, $tx_in_cfg: TxInConfiguration) $body
+
+				#[sqlx::test(migrations = "./testdata/governed-map/migrations-v1")]
+				async fn [<$name _v1>]($pool: PgPool) {
+					$name($pool, TxInConfiguration::Legacy).await
+				}
+
+				#[sqlx::test(migrations = "./testdata/governed-map/migrations-v2")]
+				async fn [<$name _v2>]($pool: PgPool) {
+					$name($pool, TxInConfiguration::Consumed).await
+				}
+			);
+		)*
+    };
 }
 
-#[sqlx::test(migrations = "./testdata/governed-map/migrations-v2")]
-async fn test_cached_governed_map_fails_on_wrong_block_hash(pool: PgPool) {
-	let source = make_cached_source(pool, TxInConfiguration::Consumed).await;
-	let mc_block =
-		McBlockHash(hex!("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"));
-	let result = source.get_mapping_changes(None, mc_block, scripts()).await;
-	assert_err!(result);
-}
+with_migration_versions! {
+	async fn test_governed_map_fails_on_wrong_block_hash(pool: PgPool, tx_in_config: TxInConfiguration) {
+		let source = make_source(pool, tx_in_config);
+		let mc_block =
+			McBlockHash(hex!("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"));
+		let result = source.get_mapping_changes(None, mc_block, scripts()).await;
+		assert_err!(result);
+	}
 
-#[sqlx::test(migrations = "./testdata/governed-map/migrations-v2")]
-async fn test_governed_map_insert(pool: PgPool) {
-	let source = make_source(pool, TxInConfiguration::Consumed);
-	let mut result = source.get_mapping_changes(None, BLOCK_1, scripts()).await.unwrap();
-	result.sort();
-	let expected = vec![
-		(
-			"key1".to_owned(),
-			Some(ByteString::from(hex!("11111111111111111111111111111111").to_vec())),
-		),
-		(
-			"key2".to_owned(),
-			Some(ByteString::from(hex!("22222222222222222222222222222222").to_vec())),
-		),
-	];
-	assert_eq!(result, expected);
-}
+	async fn test_cached_governed_map_fails_on_wrong_block_hash(pool: PgPool, tx_in_config: TxInConfiguration) {
+		let source = make_cached_source(pool, tx_in_config).await;
+		let mc_block =
+			McBlockHash(hex!("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"));
+		let result = source.get_mapping_changes(None, mc_block, scripts()).await;
+		assert_err!(result);
+	}
 
-#[sqlx::test(migrations = "./testdata/governed-map/migrations-v2")]
-async fn test_cached_governed_map_insert(pool: PgPool) {
-	let source = make_cached_source(pool, TxInConfiguration::Consumed).await;
-	let result = source.get_mapping_changes(None, BLOCK_1, scripts()).await.unwrap();
+	async fn test_governed_map_insert(pool: PgPool, tx_in_config: TxInConfiguration) {
+		let source = make_source(pool, tx_in_config);
+		let mut result = source.get_mapping_changes(None, BLOCK_1, scripts()).await.unwrap();
+		result.sort();
+		let expected = vec![
+			(
+				"key1".to_owned(),
+				Some(ByteString::from(hex!("11111111111111111111111111111111").to_vec())),
+			),
+			(
+				"key2".to_owned(),
+				Some(ByteString::from(hex!("22222222222222222222222222222222").to_vec())),
+			),
+		];
+		assert_eq!(result, expected);
+	}
 
-	let expected = vec![
-		(
-			"key2".to_owned(),
-			Some(ByteString::from(hex!("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa").to_vec())),
-		),
-		(
-			"key1".to_owned(),
-			Some(ByteString::from(hex!("11111111111111111111111111111111").to_vec())),
-		),
-		(
-			"key2".to_owned(),
-			Some(ByteString::from(hex!("22222222222222222222222222222222").to_vec())),
-		),
-	];
-	assert_eq!(result, expected);
-}
+	async fn test_cached_governed_map_insert(pool: PgPool, tx_in_config: TxInConfiguration) {
+		let source = make_cached_source(pool, tx_in_config).await;
+		let result = source.get_mapping_changes(None, BLOCK_1, scripts()).await.unwrap();
 
-#[sqlx::test(migrations = "./testdata/governed-map/migrations-v2")]
-async fn test_governed_map_delete(pool: PgPool) {
-	let source = make_source(pool, TxInConfiguration::Consumed);
-	let result = source.get_mapping_changes(Some(BLOCK_1), BLOCK_4, scripts()).await;
-	let expected = vec![("key1".to_owned(), None)];
-	assert_eq!(result.unwrap(), expected);
-}
+		let expected = vec![
+			(
+				"key2".to_owned(),
+				Some(ByteString::from(hex!("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa").to_vec())),
+			),
+			(
+				"key1".to_owned(),
+				Some(ByteString::from(hex!("11111111111111111111111111111111").to_vec())),
+			),
+			(
+				"key2".to_owned(),
+				Some(ByteString::from(hex!("22222222222222222222222222222222").to_vec())),
+			),
+		];
+		assert_eq!(result, expected);
+	}
 
-#[sqlx::test(migrations = "./testdata/governed-map/migrations-v2")]
-async fn test_cached_governed_map_delete(pool: PgPool) {
-	let source = make_cached_source(pool, TxInConfiguration::Consumed).await;
-	let result = source.get_mapping_changes(Some(BLOCK_1), BLOCK_4, scripts()).await;
-	let expected = vec![("key1".to_owned(), None)];
-	assert_eq!(result.unwrap(), expected);
-}
+	async fn test_governed_map_delete(pool: PgPool, tx_in_config: TxInConfiguration) {
+		let source = make_source(pool, tx_in_config);
+		let result = source.get_mapping_changes(Some(BLOCK_1), BLOCK_4, scripts()).await;
+		let expected = vec![("key1".to_owned(), None)];
+		assert_eq!(result.unwrap(), expected);
+	}
 
-#[sqlx::test(migrations = "./testdata/governed-map/migrations-v2")]
-async fn test_governed_map_upsert(pool: PgPool) {
-	let source = make_source(pool, TxInConfiguration::Consumed);
-	let mut result = source.get_mapping_changes(Some(BLOCK_6), BLOCK_7, scripts()).await.unwrap();
-	result.sort();
-	let expected = vec![(
-		"key3".to_owned(),
-		Some(ByteString::from(hex!("44444444444444444444444444444444").to_vec())),
-	)];
-	assert_eq!(result, expected);
-}
+	async fn test_cached_governed_map_delete(pool: PgPool, tx_in_config: TxInConfiguration) {
+		let source = make_cached_source(pool, tx_in_config).await;
+		let result = source.get_mapping_changes(Some(BLOCK_1), BLOCK_4, scripts()).await;
+		let expected = vec![("key1".to_owned(), None)];
+		assert_eq!(result.unwrap(), expected);
+	}
 
-#[sqlx::test(migrations = "./testdata/governed-map/migrations-v2")]
-async fn test_cached_governed_map_upsert(pool: PgPool) {
-	let source = make_cached_source(pool, TxInConfiguration::Consumed).await;
-	let mut result = source.get_mapping_changes(Some(BLOCK_6), BLOCK_7, scripts()).await.unwrap();
-	result.sort();
-	let expected = vec![
-		("key3".to_owned(), None),
-		(
+	async fn test_governed_map_upsert(pool: PgPool, tx_in_config: TxInConfiguration) {
+		let source = make_source(pool, tx_in_config);
+		let mut result = source.get_mapping_changes(Some(BLOCK_6), BLOCK_7, scripts()).await.unwrap();
+		result.sort();
+		let expected = vec![(
 			"key3".to_owned(),
 			Some(ByteString::from(hex!("44444444444444444444444444444444").to_vec())),
-		),
-	];
-	assert_eq!(result, expected);
+		)];
+		assert_eq!(result, expected);
+	}
+
+	async fn test_cached_governed_map_upsert(pool: PgPool, tx_in_config: TxInConfiguration) {
+		let source = make_cached_source(pool, tx_in_config).await;
+		let mut result = source.get_mapping_changes(Some(BLOCK_6), BLOCK_7, scripts()).await.unwrap();
+		result.sort();
+		let expected = vec![
+			("key3".to_owned(), None),
+			(
+				"key3".to_owned(),
+				Some(ByteString::from(hex!("44444444444444444444444444444444").to_vec())),
+			),
+		];
+		assert_eq!(result, expected);
+	}
 }
 
 fn scripts() -> MainChainScriptsV1 {
