e67b3ddcd1c0b9512e65da1d915e11441c51dfe0
ladamesny <larry.adames@iohk.io>
2025-09-10 09:27:51 -0400

    beefy upgrade: save configs for testing beefy upgrade

--- dev/local-environment/configurations/partner-chains-nodes/partner-chains-node-4/entrypoint.sh
+++ dev/local-environment/configurations/partner-chains-nodes/partner-chains-node-4/entrypoint.sh
@@ -23,6 +23,7 @@ export MC__FIRST_EPOCH_TIMESTAMP_MILLIS=$(cat /shared/MC__FIRST_EPOCH_TIMESTAMP_
   --keystore-path=/keystore \
   --unsafe-rpc-external \
   --rpc-port=9936 \
+  -lbeefy=trace \
   --rpc-cors=all \
   --prometheus-port=9618 \
   --prometheus-external \
