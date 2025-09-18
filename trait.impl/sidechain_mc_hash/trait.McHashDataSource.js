3fa6dfde3122d487be248d08173da16c8d011bde
AmbientTea <AmbientTea@users.noreply.github.com>
2025-09-18 09:16:56 +0000

    deploy: 29d91b2a0044092a58fddc5e5c08b627df10782c

--- trait.impl/sidechain_mc_hash/trait.McHashDataSource.js
+++ trait.impl/sidechain_mc_hash/trait.McHashDataSource.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["partner_chains_db_sync_data_sources",[["impl McHashDataSource for <a class=\"struct\" href=\"partner_chains_db_sync_data_sources/struct.McHashDataSourceImpl.html\" title=\"struct partner_chains_db_sync_data_sources::McHashDataSourceImpl\">McHashDataSourceImpl</a>"]]],["partner_chains_mock_data_sources",[["impl McHashDataSource for <a class=\"struct\" href=\"partner_chains_mock_data_sources/struct.McHashDataSourceMock.html\" title=\"struct partner_chains_mock_data_sources::McHashDataSourceMock\">McHashDataSourceMock</a>"]]],["sidechain_mc_hash",[]]]);
+    var implementors = Object.fromEntries([["partner_chains_db_sync_data_sources",[["impl <a class=\"trait\" href=\"sidechain_mc_hash/trait.McHashDataSource.html\" title=\"trait sidechain_mc_hash::McHashDataSource\">McHashDataSource</a> for <a class=\"struct\" href=\"partner_chains_db_sync_data_sources/struct.McHashDataSourceImpl.html\" title=\"struct partner_chains_db_sync_data_sources::McHashDataSourceImpl\">McHashDataSourceImpl</a>"]]],["partner_chains_db_sync_data_sources",[["impl McHashDataSource for <a class=\"struct\" href=\"partner_chains_db_sync_data_sources/struct.McHashDataSourceImpl.html\" title=\"struct partner_chains_db_sync_data_sources::McHashDataSourceImpl\">McHashDataSourceImpl</a>"]]],["partner_chains_mock_data_sources",[["impl <a class=\"trait\" href=\"sidechain_mc_hash/trait.McHashDataSource.html\" title=\"trait sidechain_mc_hash::McHashDataSource\">McHashDataSource</a> for <a class=\"struct\" href=\"partner_chains_mock_data_sources/struct.McHashDataSourceMock.html\" title=\"struct partner_chains_mock_data_sources::McHashDataSourceMock\">McHashDataSourceMock</a>"]]],["partner_chains_mock_data_sources",[["impl McHashDataSource for <a class=\"struct\" href=\"partner_chains_mock_data_sources/struct.McHashDataSourceMock.html\" title=\"struct partner_chains_mock_data_sources::McHashDataSourceMock\">McHashDataSourceMock</a>"]]],["sidechain_mc_hash",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[269,261,25]}
+//{"start":57,"fragment_lengths":[399,270,391,261,25]}
