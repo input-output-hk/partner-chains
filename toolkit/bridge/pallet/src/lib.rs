c84a6a43d25f3f1f0c8dabb55dc939d9f15fcbe0
Nikolaos Dymitriadis <nikolaos.dymitriadis@iohk.io>
2025-09-24 13:42:29 +0200

    organize imports

--- toolkit/bridge/pallet/src/lib.rs
+++ toolkit/bridge/pallet/src/lib.rs
@@ -216,8 +216,10 @@ pub mod pallet {
 	use frame_system::{ensure_none, pallet_prelude::OriginFor};
 	use parity_scale_codec::MaxEncodedLen;
 	use sidechain_domain::UtxoId;
-	use sp_partner_chains_bridge::{BridgeDataCheckpoint, TokenBridgeTransfersV1};
-	use sp_partner_chains_bridge::{INHERENT_IDENTIFIER, InherentError, MainChainScripts};
+	use sp_partner_chains_bridge::{
+		BridgeDataCheckpoint, INHERENT_IDENTIFIER, InherentError, MainChainScripts,
+		TokenBridgeTransfersV1,
+	};
 
 	/// Current version of the pallet
 	pub const PALLET_VERSION: u32 = 1;
