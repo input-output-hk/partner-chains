a05f7d9a76c640f1fd982d4e3cdaed7762080205
ladamesny <larry.adames@iohk.io>
2025-10-13 08:30:25 -0400

    update local env configs to extend epochLength from 30 to 60 seconds

--- dev/local-environment/configurations/cardano/entrypoint.sh
+++ dev/local-environment/configurations/cardano/entrypoint.sh
@@ -6,8 +6,8 @@ chmod 777 /shared
 
 echo "Calculating target time for synchronised chain start..."
 
-# Local env Partner Chains epochs are 30 seconds long. PC and MC epochs have to align. The following line makes MC epoch 0 start at some PC epoch start.
-target_time=$(( ($(date +%s) / 30 + 1) * 30 ))
+# Local env Partner Chains epochs are 120 seconds long. PC and MC epochs have to align. The following line makes MC epoch 0 start at some PC epoch start.
+target_time=$(( ($(date +%s) / 120 + 1) * 120 ))
 echo "$target_time" > /shared/cardano.start
 byron_startTime=$target_time
 shelley_systemStart=$(date --utc +"%Y-%m-%dT%H:%M:%SZ" --date="@$target_time")
