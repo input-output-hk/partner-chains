6faf13b85780dc38dcb13581607c9021bc96b2a9
Taylor Gunnoe <taylor.gunnoe@iohk.io>
2025-11-18 14:30:07 -0500

    fix: cleanup substrate source and fix build

--- flake.nix
+++ flake.nix
@@ -13,18 +13,13 @@
 
     flake-utils.url = "github:numtide/flake-utils";
 
-    cardano-node = {
-      url = "github:IntersectMBO/cardano-node/10.1.4";
-      flake = false;
-    };
-
-    flake-compat = {
-      url = "github:input-output-hk/flake-compat/fixes";
-      flake = false;
+    n2c = {
+      url = "github:nlewo/nix2container";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, crane, fenix, flake-utils, ... }:
+  outputs = { self, nixpkgs, crane, fenix, flake-utils, n2c, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
@@ -49,7 +44,9 @@
             pkgs.clang
           ];
           
-          BINDGEN_EXTRA_CLANG_ARGS = if pkgs.lib.hasSuffix "linux" system then "-I${pkgs.glibc.dev}/include -I${pkgs.clang.cc.lib}/lib/clang/19/include" else "";
+          BINDGEN_EXTRA_CLANG_ARGS = if pkgs.lib.hasSuffix "linux" system
+                                     then "-I${pkgs.glibc.dev}/include -I${pkgs.clang.cc.lib}/lib/clang/19/include"
+                                     else "";
           LIBCLANG_PATH = "${pkgs.clang.cc.lib}/lib";
 
           CFLAGS = if pkgs.lib.hasSuffix "linux" system then
@@ -59,9 +56,7 @@
 
           PROTOC = "${pkgs.protobuf}/bin/protoc";
           #C_INCLUDE_PATH = "${pkgs.clang.cc.lib}/lib/clang/19/include";
-          
-          # RocksDB configuration - use system library
-          ROCKSDB_LIB_DIR = "${pkgs.rocksdb}/lib/";
+
           OPENSSL_NO_VENDOR = 1;
           OPENSSL_DIR = "${pkgs.openssl.dev}";
           OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
@@ -73,18 +68,24 @@
 
         # Common build inputs for all targets
         commonArgs = {
-          pname = "partner-chains-demo-node"; 
-          src = pkgs.lib.cleanSourceWith {
-            src = self;
-            filter = path: type:
+          pname = "partner-chains-demo-node";
+          # Clean the project directory so that the nix hash
+          # doesn't change when unrelated files to builds update
+          src = let
+            jsonFilter = path: _type: builtins.match ".*\\.json$" path != null;
+            combinedFilter = path: type:
               (craneLib.filterCargoSources path type) ||
-              (pkgs.lib.hasSuffix "examples" path) ||
-              (pkgs.lib.hasSuffix ".json" path);
+              (jsonFilter path type);
+          in pkgs.lib.cleanSourceWith {
+            src = self;
+            filter = combinedFilter;
+            name = "source";
           };
           
           buildInputs = with pkgs; [
             openssl
             libclang.lib
+            stdenv.cc.cc.lib
           ] ++ pkgs.lib.optionals pkgs.stdenv.hostPlatform.isLinux [
             pkgs.rust-jemalloc-sys-unprefixed
           ] ++ pkgs.lib.optionals pkgs.stdenv.hostPlatform.isDarwin [
@@ -96,28 +97,42 @@
             pkg-config
             protobuf
             llvmPackages.lld
+            autoPatchelfHook
           ];
 
           doCheck = false;
         } // shellEnv;
 
+        cargoVendorDir = craneLib.vendorCargoDeps {
+          inherit (commonArgs) src;
+          # Remove fixture and example directories from Polkadot SDK, don't want them vendored/checked
+          # https://github.com/paritytech/polkadot-sdk/blob/polkadot-stable2412-1/.github/workflows/checks-quick.yml#L91-L97
+          overrideVendorGitCheckout = let
+            isPolkadotSdk = p: pkgs.lib.hasPrefix "git+https://github.com/paritytech/polkadot-sdk.git" p.source;
+          in ps: drv:
+            if pkgs.lib.any (p: isPolkadotSdk p) ps then
+              drv.overrideAttrs {
+                postPatch = ''
+                  rm -rf substrate/frame/contracts/fixtures/build || true
+                  rm -rf substrate/frame/contracts/fixtures/contracts/common || true
+                  rm -rf substrate/primitives/state-machine/fuzz || true
+                '';
+              }
+            else
+              drv;
+          };
         # Build the workspace dependencies separately
         cargoArtifacts = craneLib.buildDepsOnly (commonArgs // {
-          pname = "partner-chains-demo-node-deps";
+          inherit cargoVendorDir;
         });
 
         partner-chains-demo-node = craneLib.buildPackage (commonArgs // {
           pname = "partner-chains-demo-node";
           version = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).workspace.package.version;
-          
           inherit cargoArtifacts;
 
           # Git commit hash for partner-chains CLI --version flag
-          SUBSTRATE_CLI_GIT_COMMIT_HASH = self.dirtyShortRev or self.shortRev;
-          
-          postFixup = pkgs.lib.optionalString pkgs.stdenv.hostPlatform.isLinux ''
-            patchelf --set-rpath "${pkgs.rocksdb}/lib:${pkgs.stdenv.cc.cc.lib}/lib" $out/bin/partner-chains-demo-node
-          '';
+          SUBSTRATE_CLI_GIT_COMMIT_HASH = "dev"; # self.dirtyShortRev or self.shortRev;
         });
 
         cargoTest = craneLib.cargoTest (commonArgs // {
@@ -126,31 +141,20 @@
 
         cargoClippy = craneLib.cargoClippy (commonArgs // {
           inherit cargoArtifacts;
-          cargoClippyExtraArgs = "--all-targets -- --deny warnings";
         });
 
         cargoFmt = craneLib.cargoFmt {
           inherit (commonArgs) pname src;
         };
 
-      in
-      {
-        checks = { 
-          # Build the crate as part of `nix flake check`
-          inherit partner-chains-demo-node cargoTest cargoClippy cargoFmt;
-        };
-
-        packages = {
-          default = partner-chains-demo-node;
-          inherit partner-chains-demo-node;
-        };
-        devShells.default = craneLib.devShell ({
+        devShell = craneLib.devShell ({
           name = "partner-chains-demo-node-shell";
-          # Inherit inputs from checks, which pulls in the build environment from packages.default (and others)
-          checks = self.checks.${system};
+          # Inherit inputs from other build artifacts
+          inputsFrom = [ partner-chains-demo-node ];
 
           # Extra packages for the dev shell
           packages = with pkgs; [
+            attic-client
             awscli2
             bashInteractive
             cargo-edit
@@ -178,6 +182,32 @@
             [pkgs.clang]);
         } // shellEnv);
 
+      in { # Main flake outputs section
+        checks = {
+          # Build the crate as part of `nix flake check'
+          inherit partner-chains-demo-node cargoTest cargoFmt devShell;
+        };
+
+        packages = {
+          inherit partner-chains-demo-node;
+          default = partner-chains-demo-node;
+          oci-image = n2c.packages.${system}.nix2container.buildImage {
+            name = "partner-chains-demo-node";
+            config = {
+              Entrypoint = [ "${partner-chains-demo-node}/bin/partner-chains-demo-node" ];
+              Expose = [
+                "30333/tcp"
+                "9615/tcp"
+                "9933/tcp"
+                "9944/tcp"
+              ];
+              Volumes = { "/data" = {}; };
+            };
+          };
+        };
+
+        devShells.default = devShell;
+
         formatter = pkgs.nixfmt-rfc-style;
       });
 
@@ -187,12 +217,12 @@
     extra-substituters = [
       "https://nix-community.cachix.org"
       "https://cache.iog.io"
-      "https://cache.sc.iog.io"
+      "https://ci.sc.iog.io/partner-chains"
     ];
     extra-trusted-public-keys = [
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "cache.sc.iog.io:b4YIcBabCEVKrLQgGW8Fylz4W8IvvfzRc+hy0idqrWU="
+      "partner-chains:j9StpxUY/znqFqaevhQRxCH4Hi0F4rCGXDiUSjz+kew="
     ];
   };
 }
