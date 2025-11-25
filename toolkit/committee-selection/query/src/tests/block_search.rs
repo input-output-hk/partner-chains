404ad52a8e2adb248d9ca816f9a98a596a2c89f3
Nikolaos Dymitriadis <nikolaos.dymitriadis@iohk.io>
2025-11-25 13:12:30 +0100

    simplify more

--- toolkit/committee-selection/query/src/tests/block_search.rs
+++ toolkit/committee-selection/query/src/tests/block_search.rs
@@ -1,9 +1,5 @@
-mod rpc_mock;
-mod runtime_api_mock;
-
 use super::*;
 use proptest::prelude::*;
-use rpc_mock::*;
 use std::sync::Arc;
 
 use crate::tests::conversion::*;
@@ -14,7 +10,7 @@ mod find_block_number_tests {
 	proptest! {
 		#[test]
 		fn should_find_any_block_in_epoch(epoch in get_epoch(1u32)..get_epoch(BEST_NUMBER)) {
-			let client = Arc::new(TestClient { best_number: BEST_NUMBER });
+			let client = Arc::new(TestApi {});
 
 			let block_hash = client.find_any_block_in_epoch(ScEpochNumber(epoch as u64)).unwrap().into();
 			let block_number = block_hash_to_block_number(block_hash);
@@ -24,7 +20,7 @@ mod find_block_number_tests {
 
 		#[test]
 		fn should_return_error_when_no_block_in_epoch(epoch in get_epoch(BEST_NUMBER) + 1..u32::MAX) {
-			let client = Arc::new(TestClient { best_number: BEST_NUMBER });
+			let client = Arc::new(TestApi {});
 
 			assert!(client.find_any_block_in_epoch(ScEpochNumber(epoch as u64)).is_err());
 		}
