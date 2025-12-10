689230658b0cc19f584c09306dbc7ae40b5c4121
Krisztian Pinter <krisztian.pinter@iohk.io>
2025-12-10 17:08:36 +0100

    wip

--- toolkit/data-sources/dolos/src/bridge.rs
+++ toolkit/data-sources/dolos/src/bridge.rs
@@ -7,10 +7,12 @@ use blockfrost_openapi::models::{
 	tx_content_utxo::TxContentUtxo,
 };
 use cardano_serialization_lib::PlutusData;
+use partner_chains_plutus_data::bridge::{TokenTransferDatum, TokenTransferDatumV1};
 use sidechain_domain::*;
 use sp_partner_chains_bridge::{
 	BridgeDataCheckpoint, BridgeTransferV1, MainChainScripts, TokenBridgeDataSource,
 };
+use std::fmt::Debug;
 use std::marker::PhantomData;
 
 pub struct TokenBridgeDataSourceImpl<RecipientAddress> {
@@ -27,12 +29,15 @@ impl<RecipientAddress> TokenBridgeDataSourceImpl<RecipientAddress> {
 #[async_trait::async_trait]
 impl<RecipientAddress: Send + Sync> TokenBridgeDataSource<RecipientAddress>
 	for TokenBridgeDataSourceImpl<RecipientAddress>
+where
+	RecipientAddress: Debug,
+	RecipientAddress: (for<'a> TryFrom<&'a [u8]>),
 {
 	async fn get_transfers(
 		&self,
-		_main_chain_scripts: MainChainScripts,
+		main_chain_scripts: MainChainScripts,
 		data_checkpoint: BridgeDataCheckpoint,
-		_max_transfers: u32,
+		max_transfers: u32,
 		current_mc_block_hash: McBlockHash,
 	) -> Result<(Vec<BridgeTransferV1<RecipientAddress>>, BridgeDataCheckpoint)> {
 		let current_mc_block = self.client.blocks_by_id(current_mc_block_hash).await?;
@@ -56,23 +61,27 @@ impl<RecipientAddress: Send + Sync> TokenBridgeDataSource<RecipientAddress>
 			},
 		};
 
-		let asset = Asset {
+		let asset = AssetId {
 			policy_id: main_chain_scripts.token_policy_id.into(),
 			asset_name: main_chain_scripts.token_asset_name.into(),
 		};
+		let current_mc_block_height: McBlockNumber = McBlockNumber(
+			current_mc_block.height.expect("current mc block has valid height") as u32,
+		);
 		let utxos = get_bridge_utxos_tx(
+			&self.client,
 			&main_chain_scripts.illiquid_circulation_supply_validator_address.into(),
 			asset,
 			data_checkpoint,
-			current_mc_block.block_no,
+			current_mc_block_height,
 			Some(max_transfers),
 		)
 		.await?;
 
 		let new_checkpoint = match utxos.last() {
-			None => BridgeDataCheckpoint::Block(current_mc_block.block_no.into()),
+			None => BridgeDataCheckpoint::Block(current_mc_block_height),
 			Some(_) if (utxos.len() as u32) < max_transfers => {
-				BridgeDataCheckpoint::Block(current_mc_block.block_no.into())
+				BridgeDataCheckpoint::Block(current_mc_block_height)
 			},
 			Some(utxo) => BridgeDataCheckpoint::Utxo(utxo.utxo_id()),
 		};
@@ -81,6 +90,42 @@ impl<RecipientAddress: Send + Sync> TokenBridgeDataSource<RecipientAddress>
 
 		Ok((transfers, new_checkpoint))
 	}
+}
+
+fn utxo_to_transfer<RecipientAddress>(
+	utxo: BridgeUtxo,
+) -> Option<BridgeTransferV1<RecipientAddress>>
+where
+	RecipientAddress: for<'a> TryFrom<&'a [u8]>,
+{
+	let token_delta = utxo.tokens_out.0.checked_sub(utxo.tokens_in.0)?;
+
+	if token_delta == 0 {
+		return None;
+	}
+
+	let token_amount = token_delta as u64;
+
+	let Some(datum) = utxo.datum.clone() else {
+		return Some(BridgeTransferV1::InvalidTransfer { token_amount, utxo_id: utxo.utxo_id() });
+	};
+
+	let transfer = match TokenTransferDatum::try_from(datum) {
+		Ok(TokenTransferDatum::V1(TokenTransferDatumV1::UserTransfer { receiver })) => {
+			match RecipientAddress::try_from(receiver.0.as_ref()) {
+				Ok(recipient) => BridgeTransferV1::UserTransfer { token_amount, recipient },
+				Err(_) => {
+					BridgeTransferV1::InvalidTransfer { token_amount, utxo_id: utxo.utxo_id() }
+				},
+			}
+		},
+		Ok(TokenTransferDatum::V1(TokenTransferDatumV1::ReserveTransfer)) => {
+			BridgeTransferV1::ReserveTransfer { token_amount }
+		},
+		Err(_) => BridgeTransferV1::InvalidTransfer { token_amount, utxo_id: utxo.utxo_id() },
+	};
+
+	Some(transfer)
 }
 
 pub(crate) struct BridgeUtxo {
@@ -93,22 +138,48 @@ pub(crate) struct BridgeUtxo {
 	pub(crate) datum: Option<cardano_serialization_lib::PlutusData>,
 }
 
+impl BridgeUtxo {
+	pub(crate) fn utxo_id(&self) -> UtxoId {
+		UtxoId { tx_hash: self.tx_hash.into(), index: self.utxo_ix.into() }
+	}
+
+	pub(crate) fn ordering_key(&self) -> UtxoOrderingKey {
+		(self.block_number, self.tx_ix, self.utxo_ix)
+	}
+}
+
+pub(crate) type UtxoOrderingKey = (McBlockNumber, McTxIndexInBlock, UtxoIndex);
+
+#[derive(Debug, Clone, PartialEq)]
+pub(crate) struct TxBlockInfo {
+	pub(crate) block_number: McBlockNumber,
+	pub(crate) tx_ix: McTxIndexInBlock,
+}
+
 pub(crate) async fn get_block_info_for_utxo(
 	client: &MiniBFClient,
 	tx_hash: McTxHash,
 ) -> Result<Option<TxBlockInfo>> {
-
-	// SELECT
-	// 	block.block_no AS block_number,
-	// 	tx.block_index AS tx_ix
-	// FROM tx
-	// JOIN block  ON block.id = tx.block_id
-	// WHERE tx.hash = $tx_hash
+	let tx = client.transaction_by_hash(tx_hash).await?;
+	Ok(Some(TxBlockInfo {
+		block_number: McBlockNumber(tx.block_height as u32),
+		tx_ix: McTxIndexInBlock(tx.index as u32),
+	}))
 }
 
+#[derive(Clone)]
 pub(crate) enum ResolvedBridgeDataCheckpoint {
-	Utxo { block_number: BlockNumber, tx_ix: TxIndexInBlock, tx_out_ix: TxIndex },
-	Block { number: BlockNumber },
+	Utxo { block_number: McBlockNumber, tx_ix: McTxIndexInBlock, tx_out_ix: UtxoIndex },
+	Block { number: McBlockNumber },
+}
+
+impl ResolvedBridgeDataCheckpoint {
+	fn block_number(&self) -> McBlockNumber {
+		match self {
+			ResolvedBridgeDataCheckpoint::Utxo { block_number, .. } => *block_number,
+			ResolvedBridgeDataCheckpoint::Block { number } => *number,
+		}
+	}
 }
 
 pub(crate) async fn get_bridge_utxos_tx(
@@ -119,26 +190,29 @@ pub(crate) async fn get_bridge_utxos_tx(
 	to_block: McBlockNumber,
 	max_utxos: Option<u32>,
 ) -> Result<Vec<BridgeUtxo>> {
-	let x = client.assets_transactions(native_token.clone()).await?;
-	let y = x.into_iter().map(|a| async move {
-		if a.block_height as u32 >= to_block.0 {
+	let txs = client.assets_transactions(native_token.clone()).await?;
+	let checkpoint_block_no = checkpoint.block_number().0;
+	let futures = txs.into_iter().map(|a| async move {
+		let block_no = a.block_height as u32;
+		if checkpoint_block_no < block_no && block_no <= to_block.0 {
 			let tx_hash = McTxHash::from_hex_unsafe(&a.tx_hash);
-			let x = client.transactions_utxos(tx_hash).await?;
-			let y = client.transaction_by_hash(tx_hash).await?;
-			Result::Ok(Some((x, y)))
+			let utxos = client.transactions_utxos(tx_hash).await?;
+			let tx = client.transaction_by_hash(tx_hash).await?;
+			Result::Ok(Some((utxos, tx)))
 		} else {
 			Result::Ok(None)
 		}
 	});
-	let mut z = futures::future::try_join_all(y)
+	let mut bridge_utxos = futures::future::try_join_all(futures)
 		.await?
 		.iter()
 		.flatten()
-		.flat_map(|(x, y): &(TxContentUtxo, TxContent)| {
-			let inputs = x.inputs.iter().filter(|i| i.address == icp_address.to_string());
-			let outputs = x.outputs.iter().filter(|o| o.address == icp_address.to_string());
+		.flat_map(|(utxos, tx): &(TxContentUtxo, TxContent)| {
+			let inputs = utxos.inputs.iter().filter(|i| i.address == icp_address.to_string());
+			let outputs = utxos.outputs.iter().filter(|o| o.address == icp_address.to_string());
 			let native_token = native_token.clone();
-			outputs.map(move |output| {
+			let checkpoint_clone = checkpoint.clone();
+			outputs.filter_map(move |output| {
 				let native_token = native_token.clone();
 				let output_tokens = get_all_tokens(&output.amount, &native_token.clone());
 				let input_tokens = inputs
@@ -146,41 +220,36 @@ pub(crate) async fn get_bridge_utxos_tx(
 					.map(move |input| get_all_tokens(&input.amount, &native_token.clone()))
 					.sum();
 
-				BridgeUtxo {
-					block_number: McBlockNumber(y.block_height as u32),
-					tokens_out: NativeTokenAmount(output_tokens),
-					tokens_in: NativeTokenAmount(input_tokens),
-					datum: output
-						.inline_datum
-						.clone()
-						.map(|d| PlutusData::from_hex(&d).expect("valid datum")),
-					tx_ix: todo!(),
-					tx_hash: todo!(),
-					utxo_ix: todo!(),
+				match checkpoint_clone {
+					ResolvedBridgeDataCheckpoint::Utxo { tx_ix, tx_out_ix, .. }
+						if tx.block_height <= tx_ix.0 as i32
+							&& output.output_index <= tx_out_ix.0.into() =>
+					{
+						None
+					},
+					_ => Some(BridgeUtxo {
+						block_number: McBlockNumber(tx.block_height as u32),
+						tokens_out: NativeTokenAmount(output_tokens),
+						tokens_in: NativeTokenAmount(input_tokens),
+						datum: output
+							.inline_datum
+							.clone()
+							.map(|d| PlutusData::from_hex(&d).expect("valid datum")),
+						tx_ix: McTxIndexInBlock(tx.index as u32),
+						tx_hash: McTxHash::from_hex_unsafe(&tx.hash),
+						utxo_ix: UtxoIndex(output.output_index as u16),
+					}),
 				}
 			})
 		})
 		.collect::<Vec<_>>();
-
-	match checkpoint {
-		ResolvedBridgeDataCheckpoint::Block { number } => {
-			query_builder.push(&format!("AND block_no > {} ", number.0));
-		},
-		ResolvedBridgeDataCheckpoint::Utxo { block_number, tx_ix, tx_out_ix } => {
-			query_builder.push(&format!(
-				"AND (block_no, tx.block_index, outputs.index) > ({}, {}, {}) ",
-				block_number.0, tx_ix.0, tx_out_ix.0
-			));
-		},
-	}
-
-	query_builder.push("ORDER BY block.block_no, tx.block_index, outputs.index ");
+	bridge_utxos.sort_by_key(|b| b.ordering_key());
 
 	if let Some(max_utxos) = max_utxos {
-		query_builder.push(&format!("LIMIT {max_utxos}"));
+		bridge_utxos.truncate(max_utxos as usize);
 	}
 
-	Err("not implemented".into())
+	Ok(bridge_utxos)
 }
 
 fn get_all_tokens(amount: &Vec<TxContentOutputAmountInner>, asset_id: &AssetId) -> u128 {
@@ -195,35 +264,3 @@ fn get_all_tokens(amount: &Vec<TxContentOutputAmountInner>, asset_id: &AssetId) 
 		})
 		.sum()
 }
-
-// get_bridge_utxos_tx
-
-/*
-	* tx block no >= to_block
-	* input : at icp address && token kind = assetid -> get SUM input token quantity
-	* output: at icp address
-
-  SELECT
-	  block.block_no                          AS block_number
-	  , tx.block_index                          AS tx_ix
-	  , tx.hash                                 AS tx_hash
-	  , outputs.index                           AS utxo_ix
-	  , output_tokens.quantity                  AS tokens_out
-	  , coalesce(sum(input_tokens.quantity), 0) AS tokens_in
-	  , datum.value                             AS datum
-  FROM
-				tx_out      outputs
-	   JOIN     tx                            ON      outputs.tx_id = tx.id
-	   JOIN     block                         ON      tx.block_id = block.id
-	   JOIN     ma_tx_out   output_tokens     ON      output_tokens.tx_out_id = outputs.id
-	   JOIN     multi_asset                   ON      multi_asset.id = output_tokens.ident
-  LEFT JOIN     datum                         ON      datum.hash = outputs.data_hash
-  LEFT JOIN     tx_out     inputs             ON      inputs.consumed_by_tx_id = tx.id   AND inputs.address = $icp_address
-  LEFT JOIN     ma_tx_out  input_tokens       ON      input_tokens.tx_out_id = inputs.id AND input_tokens.ident = multi_asset.id
-
-  WHERE
-
-	  multi_asset.policy = $native_token.policy_id AND multi_asset.name = $native_token.policy_name
-  AND outputs.address = $icp_address
-  AND block_no <= $to_block
-*/
