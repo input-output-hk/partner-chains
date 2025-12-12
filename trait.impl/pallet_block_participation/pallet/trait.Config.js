f4f882e1adc6b51fe7e8e52c4bcbdc69a911513c
tgunnoe <tgunnoe@users.noreply.github.com>
2025-12-12 15:54:29 +0000

    deploy: 8a7a660da559dd55cab483726ceeb4a6e5480edf

--- trait.impl/pallet_block_participation/pallet/trait.Config.js
+++ trait.impl/pallet_block_participation/pallet/trait.Config.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["partner_chains_demo_runtime",[["impl <a class=\"trait\" href=\"pallet_block_participation/pallet/trait.Config.html\" title=\"trait pallet_block_participation::pallet::Config\">Config</a> for <a class=\"struct\" href=\"partner_chains_demo_runtime/struct.Runtime.html\" title=\"struct partner_chains_demo_runtime::Runtime\">Runtime</a>"]]]]);
+    var implementors = Object.fromEntries([["partner_chains_demo_runtime",[["impl Config for <a class=\"struct\" href=\"partner_chains_demo_runtime/struct.Runtime.html\" title=\"struct partner_chains_demo_runtime::Runtime\">Runtime</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[339]}
+//{"start":57,"fragment_lengths":[196]}
